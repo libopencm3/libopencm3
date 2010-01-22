699ec0fb1538a0333d1d0da60ad3206bfc4f7f1c
Piotr Esden-Tempski <piotr@esden.net>
2010-01-22 13:00:13 +0100

    Added fancyblink example for the MB525 eval board.

---
+++ examples/MB525/fancyblink/fancyblink.c
@@ -0,0 +1,91 @@
+/*
+ * This file is part of the libopenstm32 project.
+ *
+ * Copyright (C) 2009 Uwe Hermann <uwe@hermann-uwe.de>,
+ *               2010 Piotr Esden-Tempski <piotr@esden.net>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <libopenstm32.h>
+
+/* Set STM32 to 72 MHz. */
+void clock_setup(void)
+{
+	/* Select HSI as SYSCLK source. */
+	rcc_set_sysclk_source(SW_SYSCLKSEL_HSICLK);
+
+	/* Set the PLL multiplication factor to 9. */
+	rcc_set_pll_multiplication_factor(PLLMUL_PLL_CLK_MUL9);
+
+	/* Select HSI/2 as PLL source. */
+	rcc_set_pll_source(PLLSRC_HSI_CLK_DIV2);
+
+	rcc_set_pllxtpre(PLLXTPRE_HSE_CLK_DIV2);
+
+	/* Enable PLL oscillator and wait for it to stabilize. */
+	rcc_osc_on(PLL);
+	rcc_wait_for_osc_ready(PLL);
+
+	/* Select PLL as SYSCLK source. */
+	rcc_set_sysclk_source(SW_SYSCLKSEL_PLLCLK);
+}
+
+void gpio_setup(void)
+{
+	/* Enable GPIOC clock. */
+	rcc_peripheral_enable_clock(&RCC_APB2ENR, IOPCEN);
+
+	/* Set GPIO6 (in GPIO port C) to 'output push-pull'. */
+	gpio_set_mode(GPIOC, GPIO_MODE_OUTPUT_50_MHZ,
+		      GPIO_CNF_OUTPUT_PUSHPULL, GPIO6 | GPIO7 | GPIO8 | GPIO9);
+}
+
+int main(void)
+{
+	int i;
+
+	clock_setup();
+	gpio_setup();
+
+	/* Blink the LED (PC12) on the board. */
+	gpio_set(GPIOC, GPIO6);
+	while (1) {
+		gpio_toggle(GPIOC, GPIO6);	/* LED on/off */
+		gpio_toggle(GPIOC, GPIO7);	/* LED on/off */
+		for (i = 0; i < 400000; i++);	/* Wait (needs -O0 CFLAGS). */
+		gpio_toggle(GPIOC, GPIO7);	/* LED on/off */
+		gpio_toggle(GPIOC, GPIO8);	/* LED on/off */
+		for (i = 0; i < 400000; i++);	/* Wait (needs -O0 CFLAGS). */
+		gpio_toggle(GPIOC, GPIO8);	/* LED on/off */
+		gpio_toggle(GPIOC, GPIO9);	/* LED on/off */
+		for (i = 0; i < 400000; i++);	/* Wait (needs -O0 CFLAGS). */
+		gpio_toggle(GPIOC, GPIO8);	/* LED on/off */
+		gpio_toggle(GPIOC, GPIO9);	/* LED on/off */
+		for (i = 0; i < 400000; i++);	/* Wait (needs -O0 CFLAGS). */
+		gpio_toggle(GPIOC, GPIO7);	/* LED on/off */
+		gpio_toggle(GPIOC, GPIO8);	/* LED on/off */
+		for (i = 0; i < 400000; i++);	/* Wait (needs -O0 CFLAGS). */
+		gpio_toggle(GPIOC, GPIO6);	/* LED on/off */
+		gpio_toggle(GPIOC, GPIO7);	/* LED on/off */
+		for (i = 0; i < 400000; i++);	/* Wait (needs -O0 CFLAGS). */
+	}
+
+	return 0;
+}
+
+u32 *vector_table[2] __attribute__ ((section(".vectors"))) = {
+	(u32 *) 0x20000800,	/* Use 2 KB stack (0x800 bytes). */
+	(u32 *) main,		/* Use main() as reset vector for now. */
+};
