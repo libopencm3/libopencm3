9b4b4b1c83aa01da140e48c963a2920ed0cfdcc1
Uwe Hermann <uwe@hermann-uwe.de>
2010-03-02 10:59:17 +0100

    Add SPI example framework (unfinished).

---
+++ examples/stm32-h103/spi/spi.c
@@ -0,0 +1,65 @@
+/*
+ * This file is part of the libopenstm32 project.
+ *
+ * Copyright (C) 2010 Uwe Hermann <uwe@hermann-uwe.de>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <libopenstm32.h>
+
+void clock_setup(void)
+{
+	/* Select HSI as SYSCLK source. */
+	rcc_set_sysclk_source(SW_SYSCLKSEL_HSICLK);
+
+	/* Set the PLL multiplication factor to 9. */
+	rcc_set_pll_multiplication_factor(PLLMUL_PLL_CLK_MUL9);
+
+	/* Select HSI/2 as PLL source. */
+	rcc_set_pll_source(PLLSRC_HSI_CLK_DIV2);
+
+	rcc_set_pllxtpre(PLLXTPRE_HSE_CLK_DIV2);
+
+	/* Enable PLL oscillator and wait for it to stabilize. */
+	rcc_osc_on(PLL);
+	rcc_wait_for_osc_ready(PLL);
+
+	/* Select PLL as SYSCLK source. */
+	rcc_set_sysclk_source(SW_SYSCLKSEL_PLLCLK);
+}
+
+void spi_setup(void)
+{
+	/* TODO */
+}
+
+void gpio_setup(void)
+{
+	/* TODO */
+}
+
+int main(void)
+{
+	clock_setup();
+	gpio_setup();
+	spi_setup();
+
+	while (1) {
+		spi_write(SPI1, 0xaa);
+		spi_write(SPI1, 0x55);
+	}
+
+	return 0;
+}
