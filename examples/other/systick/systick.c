7c32f6333224eff28c2f5bb5725dcb47fd0084ab
Thomas Otto <tommi@viadmin.org>
2010-04-13 22:33:52 +0200

    Added little systick example.

---
+++ examples/other/systick/systick.c
@@ -0,0 +1,116 @@
+/*
+ * This file is part of the libopenstm32 project.
+ *
+ * Copyright (C) 2010 Thomas Otto <tommi@viadmin.org>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <libopenstm32/rcc.h>
+#include <libopenstm32/flash.h>
+#include <libopenstm32/gpio.h>
+#include <libopenstm32/nvic.h>
+#include <libopenstm32/systick.h>
+
+u32 temp32;
+
+/* Set STM32 to 72 MHz. HSE 16MHz */
+void clock_setup(void)
+{
+        /* enable Internal High Speed Oscillator */
+        rcc_osc_on(HSI);
+        rcc_wait_for_osc_ready(HSI);
+
+        /* Select HSI as SYSCLK source. */
+        rcc_set_sysclk_source(SW_SYSCLKSEL_HSICLK);
+
+        /* enable External High Speed Oscillator 16MHz */
+        rcc_osc_on(HSE);
+        rcc_wait_for_osc_ready(HSE);
+        rcc_set_sysclk_source(SW_SYSCLKSEL_HSECLK);
+
+        /* set prescalers for ADC, ABP1, ABP2...  make this before touching the PLL */
+        rcc_set_hpre(HPRE_SYSCLK_NODIV); //prescales the AHB clock from the SYSCLK
+        rcc_set_adcpre(ADCPRE_PLCK2_DIV6); //prescales the ADC from the APB2 clock; max 14MHz
+        rcc_set_ppre1(PPRE1_HCLK_DIV2); //prescales the APB1 from the AHB clock; max 36MHz
+        rcc_set_ppre2(PPRE2_HCLK_NODIV); //prescales the APB2 from the AHB clock; max 72MHz
+
+        /* sysclk should run with 72MHz -> 2 Waitstates ; choose 0WS from 0-24MHz, 1WS from 24-48MHz, 2WS from 48-72MHz */
+        flash_set_ws(FLASH_LATENCY_2WS);
+
+        /* Set the PLL multiplication factor to 9. -> 16MHz (external) * 9 (multiplier) / 2 (PLLXTPRE_HSE_CLK_DIV2) = 72MHz */
+        rcc_set_pll_multiplication_factor(PLLMUL_PLL_CLK_MUL9);
+
+        /* Select HSI as PLL source. */
+        rcc_set_pll_source(PLLSRC_HSE_CLK);
+
+        /* divide external frequency by 2 before entering pll (only valid/needed for HSE) */
+        rcc_set_pllxtpre(PLLXTPRE_HSE_CLK_DIV2);
+
+        /* Enable PLL oscillator and wait for it to stabilize. */
+        rcc_osc_on(PLL);
+        rcc_wait_for_osc_ready(PLL);
+
+        /* Select PLL as SYSCLK source. */
+        rcc_set_sysclk_source(SW_SYSCLKSEL_PLLCLK);
+}
+
+void gpio_setup(void)
+{
+	/* Enable GPIOB clock. */
+	rcc_peripheral_enable_clock(&RCC_APB2ENR, IOPBEN);
+
+	/* Set GPIO6/7 (in GPIO port B) to 'output push-pull' for the LEDs. */
+	gpio_set_mode(GPIOB, GPIO_MODE_OUTPUT_2_MHZ,
+	              GPIO_CNF_OUTPUT_PUSHPULL, GPIO6);
+	gpio_set_mode(GPIOB, GPIO_MODE_OUTPUT_2_MHZ,
+	              GPIO_CNF_OUTPUT_PUSHPULL, GPIO7);
+}
+
+void sys_tick_handler()
+{
+	temp32++;
+	
+	/* we call this handler every 1ms so 1000ms = 1s on/off */
+	if (temp32 == 1000) {
+		gpio_toggle(GPIOB, GPIO6); /* LED2 on/off */
+		temp32 = 0;
+	}
+}
+
+int main(void)
+{
+	clock_setup();
+	gpio_setup();
+
+	gpio_clear(GPIOB, GPIO7);	/* LED1 on */
+	gpio_set(GPIOB, GPIO6);		/* LED2 off */
+	
+	temp32 = 0;
+
+	/* 72MHz / 8 => 9000000 counts per second */
+	systick_set_clocksource(STK_CTRL_CLKSOURCE_AHB_DIV8); 
+
+	/* 9000000/9000 = 1000 overflows per second - every 1ms one interrupt */
+	systick_set_reload(9000);
+
+	systick_interrupt_enable();
+
+	/* start counting */
+	systick_counter_enable();
+	
+	while(1); /* Halt. */
+
+	return 0;
+}
