4f272b4ad4c89bef659bd253616129d2cab2c6bd
Thomas Otto <tommi@viadmin.org>
2010-05-02 20:20:04 +0200

    Added rcc clock setup function for 16mhz crystal.

--- include/libopenstm32/rcc.h
+++ include/libopenstm32/rcc.h
@@ -400,5 +400,6 @@ void rcc_set_hpre(u32 hpre);
 u32 rcc_get_system_clock_source(int i);
 void rcc_clock_setup_in_hsi_out_64mhz(void);
 void rcc_clock_setup_in_hse_8mhz_out_72mhz(void);
+void rcc_clock_setup_in_hse_16mhz_out_72mhz(void);
 
 #endif
