963588aba01c327757f73bf702466bb32224cfc6
Martin Mueller <martin@pfump.org>
2010-12-05 23:59:12 +0100

    Lisa/M board tests

---
+++ examples/lisa-m/usb_hid/hid.h
@@ -0,0 +1,38 @@
+/*
+ * This file is part of the libopenstm32 project.
+ *
+ * Copyright (C) 2010  Gareth McMullin <gareth@blacksphere.co.nz>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef __HID_H
+#define __HID_H
+
+#include <stdint.h>
+
+#define USB_CLASS_HID	3
+
+#define USB_DT_HID	0x21
+#define USB_DT_REPORT	0x22
+
+struct usb_hid_descriptor {
+	uint8_t bLength;
+	uint8_t bDescriptorType;
+	uint16_t bcdHID;
+	uint8_t bCountryCode;
+	uint8_t bNumDescriptors;
+} __attribute__((packed));
+
+#endif
+
