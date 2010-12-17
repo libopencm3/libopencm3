e2d82ff44c55c52131c407a264cfbd1f71c4b14d
Uwe Hermann <uwe@hermann-uwe.de>
2010-12-17 05:37:22 +0100

    Build fixes for ethernet.c, and hook it up.

--- lib/ethernet.c
+++ lib/ethernet.c
@@ -22,32 +22,32 @@
 void eth_smi_write(u8 phy, u8 reg, u16 data)
 {
 	/* Set PHY and register addresses for write access. */
-	*ETH_MACMIIAR &= ~(ETH_MACMIIAR_MR | ETH_MACMIIAR_PA);
-	*ETH_MACMIIAR |= (phy << 11) | (reg << 6) | ETH_MACMIIAR_MW;
+	ETH_MACMIIAR &= ~(ETH_MACMIIAR_MR | ETH_MACMIIAR_PA);
+	ETH_MACMIIAR |= (phy << 11) | (reg << 6) | ETH_MACMIIAR_MW;
 
 	/* Set register value. */
-	*ETH_MACMIIDR = data;
+	ETH_MACMIIDR = data;
 
 	/* Begin transaction. */
-	*ETH_MACMIIAR |= ETH_MACMIIAR_MB;
+	ETH_MACMIIAR |= ETH_MACMIIAR_MB;
 
 	/* Wait for not busy. */
-	while (*ETH_MACMIIAR & ETH_MACMIIAR_MB);
+	while (ETH_MACMIIAR & ETH_MACMIIAR_MB);
 }
 
 u16 eth_smi_read(u8 phy, u8 reg)
 {
 	/* Set PHY and register addresses for write access. */
-	*ETH_MACMIIAR &= ~(ETH_MACMIIAR_MR | ETH_MACMIIAR_PA | 
+	ETH_MACMIIAR &= ~(ETH_MACMIIAR_MR | ETH_MACMIIAR_PA | 
 			   ETH_MACMIAR_MW);
-	*ETH_MACMIIAR |= (phy << 11) | (reg << 6);
+	ETH_MACMIIAR |= (phy << 11) | (reg << 6);
 
 	/* Begin transaction. */
-	*ETH_MACMIIAR |= ETH_MACMIIAR_MB;
+	ETH_MACMIIAR |= ETH_MACMIIAR_MB;
 
 	/* Wait for not busy. */
-	while (*ETH_MACMIIAR & ETH_MACMIIAR_MB);
+	while (ETH_MACMIIAR & ETH_MACMIIAR_MB);
 
 	/* Set register value. */
-	return (u16)(*ETH_MACMIIDR);
+	return (u16)(ETH_MACMIIDR);
 }
