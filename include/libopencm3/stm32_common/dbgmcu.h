76700510e08007fcbb461c22e71649bfa2bf95de
Fergus Noble <fergusnoble@gmail.com>
2011-09-12 20:48:41 -0700

    Ability to choose memorymap for stm_common headers with define.

--- include/libopencm3/stm32_common/dbgmcu.h
+++ include/libopencm3/stm32_common/dbgmcu.h
@@ -20,7 +20,7 @@
 #ifndef LIBOPENCM3_STM32_DBGMCU_H
 #define LIBOPENCM3_STM32_DBGMCU_H
 
-#include <libopencm3/stm32/memorymap.h>
+#include <libopencm3/stm32_common/memorymap.h>
 #include <libopencm3/cm3/common.h>
 
 /* --- DBGMCU registers ---------------------------------------------------- */
