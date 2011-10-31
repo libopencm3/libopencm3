d17e548c6f45987fbb78603574b2b882cd97a53b
Stephen Caudle <github@doceme.com>
2011-10-31 15:01:25 -0400

    Add STM32F4 button example

---
+++ examples/stm32/f4/stm32-discovery/button/button.c
@@ -0,0 +1,79 @@
+/*
+ * This file is part of the libopencm3 project.
+ *
+ * Copyright (C) 2009 Uwe Hermann <uwe@hermann-uwe.de>,
+ * Copyright (C) 2010 Piotr Esden-Tempski <piotr@esden.net>
+ * Copyright (C) 2011 Stephen Caudle <scaudle@doceme.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <libopencm3/stm32/f4/rcc.h>
+#include <libopencm3/stm32/f4/gpio.h>
+
+u16 exti_line_state;
+
+/* Set STM32 to 168 MHz. */
+void clock_setup(void)
+{
+	rcc_clock_setup_hse_3v3(&hse_8mhz_3v3[CLOCK_3V3_168MHZ]);
+}
+
+void gpio_setup(void)
+{
+	/* Enable GPIOD clock. */
+	rcc_peripheral_enable_clock(&RCC_AHB1ENR, RCC_AHB1ENR_IOPDEN);
+
+	/* Set GPIO12 (in GPIO port D) to 'output push-pull'. */
+	gpio_mode_setup(GPIOD, GPIO_MODE_OUTPUT,
+			GPIO_PUPD_NONE,
+			GPIO12 | GPIO13 | GPIO14 | GPIO15);
+}
+
+void button_setup(void)
+{
+	/* Enable GPIOA clock. */
+	rcc_peripheral_enable_clock(&RCC_AHB1ENR, RCC_AHB1ENR_IOPAEN);
+
+	/* Set GPIO0 (in GPIO port A) to 'input open-drain'. */
+	gpio_mode_setup(GPIOA, GPIO_MODE_INPUT,
+			GPIO_PUPD_NONE,
+			GPIO0);
+}
+
+int main(void)
+{
+	int i;
+
+	clock_setup();
+	button_setup();
+	gpio_setup();
+
+	/* Blink the LED (PD12) on the board. */
+	while (1) {
+		gpio_toggle(GPIOD, GPIO12);
+
+		/* Upon button press, blink more slowly. */
+		exti_line_state = GPIOA_IDR;
+		if ((exti_line_state & (1 << 0)) != 0) {
+			for (i = 0; i < 3000000; i++)	/* Wait a bit. */
+				__asm__("nop");
+		}
+
+		for (i = 0; i < 3000000; i++)		/* Wait a bit. */
+			__asm__("nop");
+	}
+
+	return 0;
+}
