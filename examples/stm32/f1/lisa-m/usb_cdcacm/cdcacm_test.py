30d1aa957e57a3487124648b3590c28f72815c34
Piotr Esden-Tempski <piotr@esden.net>
2011-10-30 18:40:26 -0700

    Added cdcacm (usb serial) example for lisa-m with a small basic stresstesting python script.

---
+++ examples/stm32/f1/lisa-m/usb_cdcacm/cdcacm_test.py
@@ -0,0 +1,38 @@
+#! /usr/bin/env python
+
+import serial
+import signal
+import sys
+import time
+
+def signal_handler(signal, frame):
+        print 'You pressed Ctrl+C!'
+        ser.close();
+        sys.exit(0)
+
+signal.signal(signal.SIGINT, signal_handler);
+
+ser = serial.Serial('/dev/cu.usbmodemDEM1', 115200, timeout=1);
+
+error = 0
+
+tim = time.time();
+
+cycles = 0
+
+while 1==1:
+
+    ser.write("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*(){}");
+
+    buf = ser.read(75);
+
+    cycles += 1
+
+    if buf != "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*(){}" :
+        error+=1
+
+    print "received " + buf + " errors: " + str(error) + " cycles: " + str(cycles) + " runtime: " + str(time.time() - tim)
+
+    #time.sleep(0.1);
+
+ser.close();
