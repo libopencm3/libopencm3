505a05ccf48be13a79dac93c4a67f091fad833de
Stephen Caudle <github@doceme.com>
2011-10-31 15:01:35 -0400

    Add STM32F4 fancyblink example

---
+++ examples/stm32/f4/stm32-discovery/fancyblink/fancyblink.c
@@ -0,0 +1,61 @@
+/*
+ * This file is part of the libopencm3 project.
+ *
+ * Copyright (C) 2009 Uwe Hermann <uwe@hermann-uwe.de>
+ * Copyright (C) 2011 Damjan Marion <damjan.marion@gmail.com>
+ * Copyright (C) 2011 Mark Panajotovic <marko@electrontube.org>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <libopencm3/stm32/f4/rcc.h>
+#include <libopencm3/stm32/f4/gpio.h>
+
+/* Set STM32 to 168 MHz. */
+void clock_setup(void)
+{
+	rcc_clock_setup_hse_3v3(&hse_8mhz_3v3[CLOCK_3V3_168MHZ]);
+
+	/* Enable GPIOD clock. */
+	rcc_peripheral_enable_clock(&RCC_AHB1ENR, RCC_AHB1ENR_IOPDEN);
+}
+
+void gpio_setup(void)
+{
+	/* Set GPIO12-15 (in GPIO port D) to 'output push-pull'. */
+	gpio_mode_setup(GPIOD, GPIO_MODE_OUTPUT,
+			GPIO_PUPD_NONE,
+			GPIO12 | GPIO13 | GPIO14 | GPIO15);
+}
+
+int main(void)
+{
+	int i;
+
+	clock_setup();
+	gpio_setup();
+
+	/* Set two LEDs for wigwag effect when toggling. */
+	gpio_set(GPIOD, GPIO12 | GPIO14);
+
+	/* Blink the LEDs (PD12, PD13, PD14 and PD15) on the board. */
+	while (1) {
+		/* Toggle LEDs. */
+		gpio_toggle(GPIOD, GPIO12 | GPIO13 | GPIO14 | GPIO15);
+		for (i = 0; i < 6000000; i++)      /* Wait a bit. */
+			__asm__("nop");
+	}
+
+	return 0;
+}
