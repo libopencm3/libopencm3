78c93dc779b42f94f531f81757ad4a911bd3e953
chrysn <chrysn@fsfe.org>
2012-03-03 00:24:58 +0100

    add missed license headers to efm32 files
    some files had nonstandard shared copyright lines, fixed them too

--- examples/efm32/tinygecko/efm32-tg-stk3300/miniblink/miniblink.c
+++ examples/efm32/tinygecko/efm32-tg-stk3300/miniblink/miniblink.c
@@ -2,7 +2,7 @@
  * This file is part of the libopencm3 project.
  *
  * Copyright (C) 2009 Uwe Hermann <uwe@hermann-uwe.de>
- *               2012 chrysn <chrysn@fsfe.org>
+ * Copyright (C) 2012 chrysn <chrysn@fsfe.org>
  *
  * This library is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
--- lib/efm32/tinygecko/vector.c
+++ lib/efm32/tinygecko/vector.c
@@ -2,7 +2,7 @@
  * This file is part of the libopencm3 project.
  *
  * Copyright (C) 2010 Piotr Esden-Tempski <piotr@esden.net>,
- *               2012 chrysn <chrysn@fsfe.org>
+ * Copyright (C) 2012 chrysn <chrysn@fsfe.org>
  *
  * This library is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
