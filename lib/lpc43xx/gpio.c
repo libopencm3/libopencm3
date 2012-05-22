9439ce9d69afeb5e961cbc28ab581c3d4fc3fc60
Michael Ossmann <mike@ossmann.com>
2012-05-22 10:59:27 -0600

    new lib/lpc43xx, starting with copy of lpc17xx

---
+++ lib/lpc43xx/gpio.c
@@ -0,0 +1,30 @@
+/*
+ * This file is part of the libopencm3 project.
+ *
+ * Copyright (C) 2010 Uwe Hermann <uwe@hermann-uwe.de>
+ *
+ * This library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <libopencm3/lpc17xx/gpio.h>
+
+void gpio_set(u32 gpioport, u32 gpios)
+{
+	GPIO_SET(gpioport) = gpios;
+}
+
+void gpio_clear(u32 gpioport, u32 gpios)
+{
+        GPIO_CLR(gpioport) = gpios;
+}
