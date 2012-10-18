c69916ffb6d515b1dd644830ba27daef4fc75b58
chrysn <chrysn@fsfe.org>
2012-10-18 17:57:59 +0200

    integrate irq2nvic_h script in buildprocess

--- lib/cm3/vector.c
+++ lib/cm3/vector.c
@@ -23,7 +23,7 @@
 /* load optional platform dependent initialization routines */
 #include "../dispatch/vector.c"
 /* load the weak symbols for IRQ_HANDLERS */
-#include <libopencm3/dispatch/vector_nvic.h>
+#include "../dispatch/vector_nvic.c"
 
 #define WEAK __attribute__ ((weak))
 
---
+++ lib/dispatch/vector_nvic.c
@@ -0,0 +1,19 @@
+#if defined(STM32F1)
+#	include "../stm32/f1/vector_nvic.c"
+#elif defined(STM32F2)
+#	include "../stm32/f2/vector_nvic.c"
+#elif defined(STM32F4)
+#	include "../stm32/f4/vector_nvic.c"
+
+#elif defined(TINYGECKO)
+#	include "../efm32/tinygecko/vector_nvic.c"
+
+#elif defined(LPC43XX)
+#	include "../lpc43xx/vector_nvic.c"
+
+#else
+#	warning"no chipset defined; user interrupts are disabled"
+
+#define IRQ_HANDLERS
+
+#endif
