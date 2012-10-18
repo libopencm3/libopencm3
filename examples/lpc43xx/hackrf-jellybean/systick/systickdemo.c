c39c5c147dbe9e307298372f80c63570f07e8cd7
chrysn <chrysn@fsfe.org>
2012-10-18 21:09:35 +0200

    adapt lpc43xx example to generalizations

--- examples/lpc43xx/hackrf-jellybean/systick/systickdemo.c
+++ examples/lpc43xx/hackrf-jellybean/systick/systickdemo.c
@@ -20,7 +20,7 @@
 #include <libopencm3/lpc43xx/gpio.h>
 #include <libopencm3/lpc43xx/scu.h>
 #include <libopencm3/lpc43xx/cgu.h>
-#include <libopencm3/lpc43xx/nvic.h>
+#include <libopencm3/cm3/nvic.h>
 #include <libopencm3/lpc43xx/systick.h>
 #include <libopencm3/cm3/scs.h>
 
