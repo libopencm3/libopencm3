5ceb377a378203c80580fbe5160000fca998b635
chrysn <chrysn@fsfe.org>
2012-10-18 16:52:48 +0200

    switch all interrupt lists to yaml format

--- include/libopencm3/dispatch/nvic.h
+++ include/libopencm3/dispatch/nvic.h
@@ -1,9 +1,9 @@
 #if defined(STM32F1)
-#	include <libopencm3/stm32/f1/nvic_f1.h>
+#	include <libopencm3/stm32/f1/nvic.h>
 #elif defined(STM32F2)
-#	include <libopencm3/stm32/f2/nvic_f2.h>
+#	include <libopencm3/stm32/f2/nvic.h>
 #elif defined(STM32F4)
-#	include <libopencm3/stm32/f4/nvic_f4.h>
+#	include <libopencm3/stm32/f4/nvic.h>
 
 #elif defined(TINYGECKO)
 #	include <libopencm3/efm32/tinygecko/nvic.h>
