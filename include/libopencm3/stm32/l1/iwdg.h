312d887825325daf7561046bd31e3ec449c7a01c
Ken Sarkies <ksarkies@internode.on.net>
2012-12-06 02:45:17 +1030

    IWDG moved to common area

--- include/libopencm3/stm32/l1/iwdg.h
+++ include/libopencm3/stm32/l1/iwdg.h
@@ -1,7 +1,21 @@
-/* This provides unification of code over STM32F subfamilies */
+/** @defgroup iwdg_defines IWDG Defines
 
+@brief <b>Defined Constants and Types for the STM32L1xx Independent Watchdog Timer</b>
+
+@ingroup STM32L1xx_defines
+
+@version 1.0.0
+
+@author @htmlonly &copy; @endhtmlonly 2010 Thomas Otto <tommi@viadmin.org>
+
+@date 18 August 2012
+
+LGPL License Terms @ref lgpl_license
+ */
 /*
  * This file is part of the libopencm3 project.
+ *
+ * Copyright (C) 2010 Thomas Otto <tommi@viadmin.org>
  *
  * This library is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
@@ -17,15 +31,11 @@
  * along with this library.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#if defined(STM32F1)
-#       include <libopencm3/stm32/f1/iwdg.h>
-#elif defined(STM32F2)
-#       include <libopencm3/stm32/f2/iwdg.h>
-#elif defined(STM32F4)
-#       include <libopencm3/stm32/f4/iwdg.h>
-#elif defined(STM32L1)
-#       include <libopencm3/stm32/l1/iwdg.h>
-#else
-#       error "stm32 family not defined."
+#ifndef LIBOPENCM3_IWDG_H
+#define LIBOPENCM3_IWDG_H
+
+#include <libopencm3/stm32/memorymap.h>
+#include <libopencm3/stm32/common/iwdg_common_all.h>
+
 #endif
 
