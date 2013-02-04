69902568d1a27f7ff7c8dbe5bf0229499de852a4
Karl Palsson <karlp@remake.is>
2013-02-04 17:20:59 +0000

    [stm32-l1] Turn on the DMA code.
    It appears to be the same dma peripheral as in the F1 and F3, so just
    enable it as is.

--- include/libopencm3/stm32/l1/dma.h
+++ include/libopencm3/stm32/l1/dma.h
@@ -1,4 +1,18 @@
-/* This provides unification of code over STM32F subfamilies */
+/** @defgroup STM32L1xx_dma_defines DMA Defines
+
+@ingroup STM32L1xx_defines
+
+@brief Defined Constants and Types for the STM32L1xx DMA Controller
+
+@version 1.0.0
+
+@author @htmlonly &copy; @endhtmlonly 2011 Fergus Noble <fergusnoble@gmail.com>
+@author @htmlonly &copy; @endhtmlonly 2012 Ken Sarkies <ksarkies@internode.on.net>
+
+@date 18 October 2012
+
+LGPL License Terms @ref lgpl_license
+ */
 
 /*
  * This file is part of the libopencm3 project.
@@ -17,15 +31,11 @@
  * along with this library.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#if defined(STM32F1)
-#       include <libopencm3/stm32/f1/dma.h>
-#elif defined(STM32F2)
-#       include <libopencm3/stm32/f2/dma.h>
-#elif defined(STM32F4)
-#       include <libopencm3/stm32/f4/dma.h>
-#elif defined(STM32L1)
-#       include <libopencm3/stm32/l1/dma.h>
-#else
-#       error "stm32 family not defined."
+#ifndef LIBOPENCM3_DMA_H
+#define LIBOPENCM3_DMA_H
+
+#include <libopencm3/stm32/l1/memorymap.h>
+#include <libopencm3/stm32/common/dma_common_f13.h>
+
 #endif
 
