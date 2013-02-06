7afc13930f86f226cac27abc63a68afed0b119ec
Karl Palsson <karlp@remake.is>
2013-02-06 11:47:23 +0000

    [stm32] Use common memorymap include for consistency
    Most peripheral headers simply include <libopencm3/stm32/memorymap.h>
    which, like the rest of libopencm3 requires the correct compiler define
    flag to be set.  A few peripherals were directly including the platform
    include, libopencm3/stm32/xx/memorymap.h, and in some of those cases it
    wasn't even correctly including the correct platform.  (Likely the
    result of copy/paste errors)
    
    These direct includes have been eliminated

--- include/libopencm3/stm32/f2/rng.h
+++ include/libopencm3/stm32/f2/rng.h
@@ -18,7 +18,7 @@
 #ifndef LIBOPENCM3_RNG_F2_H
 #define LIBOPENCM3_RNG_F2_H
 
-#include <libopencm3/stm32/f2/memorymap.h>
+#include <libopencm3/stm32/memorymap.h>
 #include <libopencm3/stm32/common/rng_common_f24.h>
 
 #endif
