be874f065aab03aafdacf17796371833ac754bbe
Federico Ruiz Ugalde <fruiz@eie.ucr.ac.cr>
2013-06-25 20:15:22 -0600

    Memory map for f3 added.

--- include/libopencm3/stm32/memorymap.h
+++ include/libopencm3/stm32/memorymap.h
@@ -24,6 +24,8 @@
 #	include <libopencm3/stm32/f1/memorymap.h>
 #elif defined(STM32F2)
 #	include <libopencm3/stm32/f2/memorymap.h>
+#elif defined(STM32F3)
+#	include <libopencm3/stm32/f3/memorymap.h>
 #elif defined(STM32F4)
 #	include <libopencm3/stm32/f4/memorymap.h>
 #elif defined(STM32L1)
