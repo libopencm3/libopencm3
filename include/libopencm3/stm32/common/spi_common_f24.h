18da63879c0ff43f920abb7619b255a09101df00
Piotr Esden-Tempski <piotr@esden.net>
2013-07-04 15:07:21 -0700

    [STM32] Rename the f0124 files to f124.
    We don't support f0 yet so let's not fool anyone. We may rename those
    files back again if when we cross check that it is actually true this
    file supports f0.

--- include/libopencm3/stm32/common/spi_common_f24.h
+++ include/libopencm3/stm32/common/spi_common_f24.h
@@ -34,7 +34,7 @@ specific memorymap.h header before including this header file.*/
 
 /**@{*/
 
-#include <libopencm3/stm32/common/spi_common_f0124.h>
+#include <libopencm3/stm32/common/spi_common_f124.h>
 
 /*
  * This file extends the common STM32 version with definitions only
