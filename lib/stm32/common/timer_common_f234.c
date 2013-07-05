7085867db675691371f6ef878c8afdf63ad903a8
Piotr Esden-Tempski <piotr@esden.net>
2013-07-04 19:03:43 -0700

    [STM32F3] Split timer common f24 into f234 and f24.

--- lib/stm32/common/timer_common_f234.c
+++ lib/stm32/common/timer_common_f234.c
@@ -27,27 +27,32 @@
 #include <libopencm3/stm32/timer.h>
 
 /*---------------------------------------------------------------------------*/
-/** @brief Set Timer Option
+/** @brief Set Input Polarity
 
-Set timer options register on TIM2 or TIM5, used for trigger remapping on TIM2,
-and similarly for TIM5 for oscillator calibration purposes.
+The timer channel must be set to input capture mode.
 
 @param[in] timer_peripheral Unsigned int32. Timer register address base
-@returns Unsigned int32. Option flags TIM2: @ref tim2_opt_trigger_remap, TIM5:
-@ref tim5_opt_trigger_remap.
+@param[in] ic ::tim_ic_id. Input Capture channel designator.
+@param[in] pol ::tim_ic_pol. Input Capture polarity control.
 */
 
-void timer_set_option(uint32_t timer_peripheral, uint32_t option)
+void timer_ic_set_polarity(uint32_t timer_peripheral, enum tim_ic_id ic,
+			   enum tim_ic_pol pol)
 {
-	if (timer_peripheral == TIM2) {
-		TIM_OR(timer_peripheral) &= ~TIM2_OR_ITR1_RMP_MASK;
-		TIM_OR(timer_peripheral) |= option;
-	} else if (timer_peripheral == TIM5) {
-		TIM_OR(timer_peripheral) &= ~TIM5_OR_TI4_RMP_MASK;
-		TIM_OR(timer_peripheral) |= option;
+	/* Clear CCxP and CCxNP to zero. For both edge trigger both fields are
+	 * set. Case 10 is invalid.
+	 */
+	TIM_CCER(timer_peripheral) &= ~(0x6 << (ic * 4));
+	switch (pol) {
+	case TIM_IC_RISING:  /* 00 */
+		break;
+	case TIM_IC_BOTH:    /* 11 */
+		TIM_CCER(timer_peripheral) |= (0x6 << (ic * 4));
+		break;
+	case TIM_IC_FALLING: /* 01 */
+		TIM_CCER(timer_peripheral) |= (0x2 << (ic * 4));
 	}
 }
-
 /**@}*/
 
 
