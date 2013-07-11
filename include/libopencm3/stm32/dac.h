eb5e40db5de107a7d1bc7895b312295a3a5a2fd8
BuFran <BuFran@seznam.cz>
2013-07-11 12:14:58 +0200

    [STM32F0:DAC] Add register definitions

--- include/libopencm3/stm32/dac.h
+++ include/libopencm3/stm32/dac.h
@@ -16,8 +16,9 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with this library.  If not, see <http://www.gnu.org/licenses/>.
  */
-
-#if defined(STM32F1)
+#if defined(STM32F0)
+#       include <libopencm3/stm32/f0/dac.h>
+#elif defined(STM32F1)
 #       include <libopencm3/stm32/f1/dac.h>
 #elif defined(STM32F2)
 #       include <libopencm3/stm32/f2/dac.h>
