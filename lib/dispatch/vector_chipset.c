822654b16b8afa98f42aa32f59aef2f90adc118d
Karl Palsson <karlp@tweak.net.au>
2013-09-11 21:39:58 +0000

    [stm32f3] Properly turn on the FPU
    The vector chipset file was added, but not included in the dispatcher.
    Fixes github issue #201

--- lib/dispatch/vector_chipset.c
+++ lib/dispatch/vector_chipset.c
@@ -1,6 +1,7 @@
-#if defined(STM32F4)
+#if defined(STM32F3)
+#	include "../stm32/f3/vector_chipset.c"
+#elif defined(STM32F4)
 #	include "../stm32/f4/vector_chipset.c"
-
 #elif defined(LPC43XX_M4)
 #	include "../lpc43xx/m4/vector_chipset.c"
 
