53c1d2c8d92e055f02b56e589e9f098a42a1e76c
Karl Palsson <karlp@tweak.net.au>
2014-04-20 14:49:05 +0530

    usb: get_configuration should return bConfigurationValue
    Rather than the index of the config. This happens to be the same
    thing if you have zero based sequential configurations.

--- lib/usb/usb_standard.c
+++ lib/usb/usb_standard.c
@@ -310,7 +310,8 @@ static int usb_standard_get_configuration(usbd_device *usbd_dev,
 	if (*len > 1) {
 		*len = 1;
 	}
-	(*buf)[0] = usbd_dev->current_config;
+	const struct usb_config_descriptor *cfg = &usbd_dev->config[usbd_dev->current_config - 1];
+	(*buf)[0] = cfg->bConfigurationValue;
 
 	return 1;
 }
