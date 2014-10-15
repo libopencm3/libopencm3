eee5a45019967b96d0930769dbb2d247eb60eddb
Frantisek Burian <BuFran@seznam.cz>
2014-10-15 17:05:56 +0200

    [stm32l0] Integrate the L0 architecture to the doxygen documentation
    Conflicts:
    	doc/Makefile

---
+++ include/libopencm3/stm32/l0/doc-stm32l0.h
@@ -0,0 +1,32 @@
+/** @mainpage libopencm3 STM32L0
+
+@version 1.0.0
+
+@date 15 November 2014
+
+API documentation for ST Microelectronics STM32L0 Cortex M0 series.
+
+LGPL License Terms @ref lgpl_license
+*/
+
+/** @defgroup STM32L0xx STM32L0xx
+Libraries for ST Microelectronics STM32L0xx series.
+
+@version 1.0.0
+
+@date 15 November 2014
+
+LGPL License Terms @ref lgpl_license
+*/
+
+/** @defgroup STM32L0xx_defines STM32L0xx Defines
+
+@brief Defined Constants and Types for the STM32L0xx series
+
+@version 1.0.0
+
+@date 7 September 2012
+
+LGPL License Terms @ref lgpl_license
+*/
+
