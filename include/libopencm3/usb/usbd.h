54b117c5a5767df8dc114612dfd8e8cb88b72c6b
Karl Palsson <karlp@tweak.net.au>
2015-05-27 03:49:51 +0530

    usb: Use enumerated return codes
    The enum usbd_request_return_codes has been available for some time.  It
    should be used internally, not just by users of this code.

--- include/libopencm3/usb/usbd.h
+++ include/libopencm3/usb/usbd.h
@@ -107,7 +107,8 @@ extern void usbd_register_sof_callback(usbd_device *usbd_dev,
 typedef void (*usbd_control_complete_callback)(usbd_device *usbd_dev,
 		struct usb_setup_data *req);
 
-typedef int (*usbd_control_callback)(usbd_device *usbd_dev,
+typedef enum usbd_request_return_codes (*usbd_control_callback)(
+		usbd_device *usbd_dev,
 		struct usb_setup_data *req, uint8_t **buf, uint16_t *len,
 		usbd_control_complete_callback *complete);
 
