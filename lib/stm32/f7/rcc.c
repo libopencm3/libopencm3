3fc0c9d0013ce65f387e5b30e56e1bdaa9ea2d91
Sync <github@sync-hv.de>
2015-10-22 13:29:43 +0200

    stm32f7: rcc: intial stubs support
    Reviewed-by: Karl Palsson <karlp@tweak.net.au>
    (Added defines and used them)

---
+++ lib/stm32/f7/rcc.c
@@ -0,0 +1,11 @@
+#include <libopencm3/stm32/rcc.h>
+
+uint32_t rcc_ahb_frequency = 16000000;
+uint32_t rcc_apb1_frequency = 16000000;
+uint32_t rcc_apb2_frequency = 16000000;
+
+uint32_t rcc_system_clock_source(void)
+{
+	/* Return the clock source which is used as system clock. */
+	return (RCC_CFGR >> RCC_CFGR_SWS_SHIFT) & RCC_CFGR_SWS_MASK;
+}
