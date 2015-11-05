4ef9830f2dc4fbce4d9591d8265ed9550a42c74b
Karl Palsson <karlp@tweak.net.au>
2015-11-05 23:31:41 +0000

    stm32: adc-v2: Pull up more common basic functionality
    Pull up eoc/eos/read_regular functions.  More simple, basic core functionality.

--- include/libopencm3/stm32/f3/adc.h
+++ include/libopencm3/stm32/f3/adc.h
@@ -609,11 +609,8 @@ void adc_set_watchdog_high_threshold(uint32_t adc, uint8_t threshold);
 void adc_set_watchdog_low_threshold(uint32_t adc, uint8_t threshold);
 void adc_set_regular_sequence(uint32_t adc, uint8_t length, uint8_t channel[]);
 void adc_set_injected_sequence(uint32_t adc, uint8_t length, uint8_t channel[]);
-bool adc_eoc(uint32_t adc);
 bool adc_eoc_injected(uint32_t adc);
-bool adc_eos(uint32_t adc);
 bool adc_eos_injected(uint32_t adc);
-uint32_t adc_read_regular(uint32_t adc);
 uint32_t adc_read_injected(uint32_t adc, uint8_t reg);
 void adc_set_injected_offset(uint32_t adc, uint8_t reg, uint32_t offset);
 
