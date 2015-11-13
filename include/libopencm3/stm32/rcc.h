8afc983f3ea4742c54777f38bea64b081eb73be0
Karl Palsson <karlp@tweak.net.au>
2015-11-13 01:04:05 +0000

    stm32l4: Add RCC definitions
    Bring in the core common code, and now that it's used, pull in the common
    memorymap

--- include/libopencm3/stm32/rcc.h
+++ include/libopencm3/stm32/rcc.h
@@ -34,6 +34,8 @@
 #       include <libopencm3/stm32/l0/rcc.h>
 #elif defined(STM32L1)
 #       include <libopencm3/stm32/l1/rcc.h>
+#elif defined(STM32L4)
+#       include <libopencm3/stm32/l4/rcc.h>
 #else
 #       error "stm32 family not defined."
 #endif
