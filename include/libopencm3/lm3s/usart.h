3c919b745e8b748254a478a3e5521bf4e29e7563
Daniele Lacamera <root@danielinux.net>
2015-11-25 11:28:29 +0100

    changed UART_SR -> USART_SR for lm3s for consistency

--- include/libopencm3/lm3s/usart.h
+++ include/libopencm3/lm3s/usart.h
@@ -81,8 +81,8 @@
 #define USART_IC_RX     		(0x01 << 4)
 /* Bits [3:0] - Reserved */
 
-#define UART_SR_RXNE  USART_IC_RX
-#define UART_SR_TXE   USART_IC_TX
+#define USART_SR_RXNE  USART_IC_RX
+#define USART_SR_TXE   USART_IC_TX
 
 enum usart_stopbits {
 	USART_STOPBITS_1,
