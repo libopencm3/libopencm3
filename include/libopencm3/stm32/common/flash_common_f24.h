ef18ca74f827470e82056865030cd160934c623f
Cem Basoglu <cem.basoglu@web.de>
2016-02-29 10:28:40 +0100

    STM32F3: flash programming operations added

--- include/libopencm3/stm32/common/flash_common_f24.h
+++ include/libopencm3/stm32/common/flash_common_f24.h
@@ -132,7 +132,6 @@ void flash_erase_all_sectors(uint32_t program_size);
 void flash_erase_sector(uint8_t sector, uint32_t program_size);
 void flash_program_double_word(uint32_t address, uint64_t data);
 void flash_program_word(uint32_t address, uint32_t data);
-void flash_program_half_word(uint32_t address, uint16_t data);
 void flash_program_byte(uint32_t address, uint8_t data);
 void flash_program(uint32_t address, uint8_t *data, uint32_t len);
 void flash_program_option_bytes(uint32_t data);
--- lib/stm32/f3/flash.c
+++ lib/stm32/f3/flash.c
@@ -50,14 +50,105 @@
 /*---------------------------------------------------------------------------*/
 /** @brief Clear All Status Flags
 
-Clears program error, end of operation, busy flags.
-*/
+ Program error, end of operation, write protect error, busy.
+ */
 
 void flash_clear_status_flags(void)
 {
 	flash_clear_pgperr_flag();
 	flash_clear_eop_flag();
+	flash_clear_wrprterr_flag();
 	flash_clear_bsy_flag();
 }
+
+/*---------------------------------------------------------------------------*/
+/** @brief Read All Status Flags
+
+ The programming error, end of operation, write protect error and busy flags
+ are returned in the order of appearance in the status register.
+
+ @returns uint32_t. bit 0: busy, bit 2: programming error, bit 4: write protect
+ error, bit 5: end of operation.
+ */
+
+uint32_t flash_get_status_flags(void)
+{
+	return FLASH_SR & (FLASH_SR_PGPERR |
+					   FLASH_SR_EOP |
+					   FLASH_SR_WRPRTERR |
+					   FLASH_SR_BSY);
+}
+
+/*---------------------------------------------------------------------------*/
+/** @brief Program a Half Word to FLASH
+
+ This performs all operations necessary to program a 16 bit word to FLASH memory.
+ The program error flag should be checked separately for the event that memory
+ was not properly erased.
+
+ Status bit polling is used to detect end of operation.
+
+ @param[in] address Full address of flash half word to be programmed.
+ @param[in] data half word to write
+ */
+
+void flash_program_half_word(uint32_t address, uint16_t data)
+{
+	flash_wait_for_last_operation();
+
+	FLASH_CR |= FLASH_CR_PG;
+
+	MMIO16(address) = data;
+
+	flash_wait_for_last_operation();
+
+	FLASH_CR &= ~FLASH_CR_PG;
+}
+
+/*---------------------------------------------------------------------------*/
+/** @brief Erase a Page of FLASH
+
+ This performs all operations necessary to erase a page in FLASH memory.
+ The page should be checked to ensure that it was properly erased. A page must
+ first be fully erased before attempting to program it.
+
+ Note that the page sizes differ between devices. See the reference manual or
+ the FLASH programming manual for details.
+
+ @param[in] page_address Full address of flash page to be erased.
+ */
+
+void flash_erase_page(uint32_t page_address)
+{
+	flash_wait_for_last_operation();
+
+	FLASH_CR |= FLASH_CR_PER;
+	FLASH_AR = page_address;
+	FLASH_CR |= FLASH_CR_STRT;
+
+	flash_wait_for_last_operation();
+
+	FLASH_CR &= ~FLASH_CR_PER;
+}
+
+/*---------------------------------------------------------------------------*/
+/** @brief Erase All FLASH
+
+ This performs all operations necessary to erase all user pages in the FLASH
+ memory. The information block is unaffected.
+ */
+
+void flash_erase_all_pages(void)
+{
+	flash_wait_for_last_operation();
+
+	FLASH_CR |= FLASH_CR_MER; /* Enable mass erase. */
+	FLASH_CR |= FLASH_CR_STRT; /* Trigger the erase. */
+
+	flash_wait_for_last_operation();
+	FLASH_CR &= ~FLASH_CR_MER; /* Disable mass erase. */
+
+}
+
 /**@}*/
 
