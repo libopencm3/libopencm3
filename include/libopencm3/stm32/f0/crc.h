34f57ae06eb4be605a9941de1aa5485f244731fd
Cem Basoglu <cem.basoglu@web.de>
2016-03-04 12:57:06 +0100

    stm32: crc-v2: STM32F0/3 extended crc unit
    Implementation of extended crc unit in f0 and f3

--- include/libopencm3/stm32/f0/crc.h
+++ include/libopencm3/stm32/f0/crc.h
@@ -32,58 +32,7 @@
 
 #ifndef LIBOPENCM3_CRC_H
 #define LIBOPENCM3_CRC_H
-/**@{*/
 
-#include <libopencm3/stm32/common/crc_common_all.h>
-
-/*****************************************************************************/
-/* Module definitions                                                        */
-/*****************************************************************************/
-
-/*****************************************************************************/
-/* Register definitions                                                      */
-/*****************************************************************************/
-
-/* Initial CRC Value */
-#define CRC_INIT			MMIO32(CRC_BASE + 0x10)
-
-/* CRC Polynomial */
-#define CRC_POL				MMIO32(CRC_BASE + 0x14)
-
-/*****************************************************************************/
-/* Register values                                                           */
-/*****************************************************************************/
-
-#define CRC_CR_REV_OUT			(1 << 7)
-
-#define CRC_CR_REV_IN_SHIFT		5
-#define CRC_CR_REV_IN			(3 << CRC_CR_REV_IN_SHIFT)
-#define CRC_CR_REV_IN_NONE		(0 << CRC_CR_REV_IN_SHIFT)
-#define CRC_CR_REV_IN_BYTE		(1 << CRC_CR_REV_IN_SHIFT)
-#define CRC_CR_REV_IN_HALF		(2 << CRC_CR_REV_IN_SHIFT)
-#define CRC_CR_REV_IN_WORD		(3 << CRC_CR_REV_IN_SHIFT)
-
-#define CRC_CR_POLYSIZE_SHIFT		3
-#define CRC_CR_POLYSIZE			(3 << CRC_CR_POLYSIZE_SHIFT)
-#define CRC_CR_POLYSIZE_32BIT		(0 << CRC_CR_POLYSIZE_SHIFT)
-#define CRC_CR_POLYSIZE_16BIT		(1 << CRC_CR_POLYSIZE_SHIFT)
-#define CRC_CR_POLYSIZE_8BIT		(2 << CRC_CR_POLYSIZE_SHIFT)
-#define CRC_CR_POLYSIZE_7BIT		(3 << CRC_CR_POLYSIZE_SHIFT)
-
-/* Default polynomial */
-#define CRC_POL_DEFAULT			0x04C11DB7
-
-/*****************************************************************************/
-/* API definitions                                                           */
-/*****************************************************************************/
-
-/*****************************************************************************/
-/* API Functions                                                             */
-/*****************************************************************************/
-
-BEGIN_DECLS
-
-END_DECLS
-/**@}*/
+#include <libopencm3/stm32/common/crc_v2.h>
 
 #endif
--- include/libopencm3/stm32/f3/crc.h
+++ include/libopencm3/stm32/f3/crc.h
@@ -32,39 +32,6 @@
 #ifndef LIBOPENCM3_CRC_H
 #define LIBOPENCM3_CRC_H
 
-#include <libopencm3/stm32/common/crc_common_all.h>
-
-/* --- CRC registers ------------------------------------------------------- */
-
-/* Initial CRC value (CRC_INIT) */
-#define CRC_INIT			MMIO32(CRC_BASE + 0x10)
-
-/* CRC polynomial (CRC_POL) */
-#define CRC_POL				MMIO32(CRC_BASE + 0x14)
-
-/* --- CRC_CR values ------------------------------------------------------- */
-
-/* REV_OUT: Reverse output data */
-#define CRC_CR_REV_OUT			(1 << 7)
-
-/* REV_IN[1:0]: Reverse input data */
-#define CRC_CR_REV_IN_NOT_AFFECTED	(0x0 << 5)
-#define CRC_CR_REV_IN_BYTE		(0x1 << 5)
-#define CRC_CR_REV_IN_HALF_WORD		(0x2 << 5)
-#define CRC_CR_REV_IN_WORD		(0x3 << 5)
-
-/* POLYSIZE[1:0]: Polynomial size */
-#define CRC_CR_POLYSIZE_32		(0x0 << 3)
-#define CRC_CR_POLYSIZE_16		(0x1 << 3)
-#define CRC_CR_POLYSIZE_8		(0x2 << 3)
-#define CRC_CR_POLYSIZE_7		(0x3 << 3)
-
-/* --- CRC_INIT values ----------------------------------------------------- */
-
-/* Bits 31:0 CRC_INIT: Programmable initial CRC value */
-
-/* --- CRC_POL values ------------------------------------------------------ */
-
-/* Bits 31:0 POL[31:0]: Programmable polynomial */
+#include <libopencm3/stm32/common/crc_v2.h>
 
 #endif
