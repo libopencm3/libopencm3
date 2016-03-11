0550c893444829bf12c904744ab23ca5b90dcbcf
Benjamin Levine <benjaminmileslevine@gmail.com>
2016-03-11 08:14:10 +0000

    Basic infrastructure for l4

---
+++ include/libopencm3/stm32/l4/doc-stm32l4.h
@@ -0,0 +1,32 @@
+/** @mainpage libopencm3 STM32L4
+
+@version 1.0.0
+
+@date 12 February 2015
+
+API documentation for ST Microelectronics STM32L4 Cortex M4 series.
+
+LGPL License Terms @ref lgpl_license
+*/
+
+/** @defgroup STM32L4xx STM32L4xx
+Libraries for ST Microelectronics STM32L4xx series.
+
+@version 1.0.0
+
+@date 12 February 2015
+
+LGPL License Terms @ref lgpl_license
+*/
+
+/** @defgroup STM32L4xx_defines STM32L4xx Defines
+
+@brief Defined Constants and Types for the STM32L4xx series
+
+@version 1.0.0
+
+@date 12 February 2015
+
+LGPL License Terms @ref lgpl_license
+*/
+
