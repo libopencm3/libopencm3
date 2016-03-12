6f30e76c61ceab409302117f1bb66d72f43a0de4
Alexandru Gagniuc <mr.nuke.me@gmail.com>
2016-03-12 10:06:48 -0800

    lm4f: Enable FPU using the vector_chipset hook
    Newlib and arm-non-eabi-gcc likes to use the FPU by default on
    Cortex-M4F chips. AS a result, do the right thing and enable the FPU
    by default.
    
    This fixes issues where code is generated which uses the FPU and
    causes the CPU to hard-fault. This change removes the responsibility
    of FPU initialization from the application code.
    
    This makes the lm4f consistent with other M4+ devices that enable the
    FPU in core library startup code.
    
    Signed-off-by: Karl Palsson <karlp@tweak.net.au>

--- lib/dispatch/vector_chipset.c
+++ lib/dispatch/vector_chipset.c
@@ -6,6 +6,8 @@
 #	include "../stm32/f7/vector_chipset.c"
 #elif defined(STM32L4)
 #	include "../stm32/l4/vector_chipset.c"
+#elif defined(LM4F)
+#	include "../lm4f/vector_chipset.c"
 #elif defined(LPC43XX_M4)
 #	include "../lpc43xx/m4/vector_chipset.c"
 #elif defined(VF6XX)
