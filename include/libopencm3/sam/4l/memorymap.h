ca1eddbc0441d8d18fa4158b8170abbd50ab6ae6
Maxim Sloyko <m.sloyko+github@gmail.com>
2016-04-16 09:38:08 -0700

    Forgot to commit memorymap

--- include/libopencm3/sam/4l/memorymap.h
+++ include/libopencm3/sam/4l/memorymap.h
@@ -22,29 +22,67 @@
 
 #include <libopencm3/cm3/common.h>
 
-/* --- SAM4L peripheral space -------------------------------------------- */
-#define PBA_BASE			(0x40000000U)
-#define PBB_BASE			(0x400A0000U)
-#define AESA_BASE			(0x400B0000U)
-#define PBC_BASE			(0x400E0000U)
-#define PBD_BASE			(0x400F0000U)
+#define PERIPH_BASE			(0x40000000U)
 
-/* --- SAM4L system controller space ------------------------------------- */
-#define SMC_BASE			(0x400E0000U)
-#define MATRIX_BASE			(0x400E0200U)
-#define PMC_BASE			(0x400E0400U)
-#define UART0_BASE			(0x400E0600U)
-#define CHIPID_BASE			(0x400E0740U)
-#define UART1_BASE			(0x400E0800U)
-#define EEFC_BASE			(0x400E0A00U)
-#define PIOA_BASE			(0x400E0E00U)
-#define PIOB_BASE			(0x400E1000U)
-#define PIOC_BASE			(0x400E1200U)
-#define RSTC_BASE			(0x400E1400U)
-#define SUPC_BASE			(0x400E1410U)
-#define RTT_BASE			(0x400E1430U)
-#define WDT_BASE			(0x400E1450U)
-#define RTC_BASE			(0x400E1460U)
-#define GPBR_BASE			(0x400E1490U)
+/* --- SAM4L peripheral bridge A -------------------------------------------- */
+#define PERIPH_PBA_BASE			PERIPH_BASE
+/* (PERIPH_PBA_BASE + 0x4000*0) RESERVED */
+#define I2SC_BASE			(PERIPH_PBA_BASE + 0x4000*1)
+#define SPI_BASE			(PERIPH_PBA_BASE + 0x4000*2)
+/* (PERIPH_PBA_BASE + 0x4000*3) RESERVED */
+#define TC0_BASE			(PERIPH_PBA_BASE + 0x4000*4)
+#define TC1_BASE			(PERIPH_PBA_BASE + 0x4000*5)
+#define TWIMS0_BASE			(PERIPH_PBA_BASE + 0x4000*6)
+#define TWIMS1_BASE			(PERIPH_PBA_BASE + 0x4000*7)
+/* (PERIPH_PBA_BASE + 0x4000*8) RESERVED */
+#define USART0_BASE			(PERIPH_PBA_BASE + 0x4000*9)
+#define USART1_BASE			(PERIPH_PBA_BASE + 0x4000*10)
+#define USART2_BASE			(PERIPH_PBA_BASE + 0x4000*11)
+#define USART3_BASE			(PERIPH_PBA_BASE + 0x4000*12)
+/* (PERIPH_PBA_BASE + 0x4000*13) RESERVED */
+#define ADCIFE_BASE			(PERIPH_PBA_BASE + 0x4000*14)
+#define DACC_BASE			(PERIPH_PBA_BASE + 0x4000*15)
+#define ACIFC_BASE			(PERIPH_PBA_BASE + 0x4000*16)
+/* (PERIPH_PBA_BASE + 0x4000*17) RESERVED */
+#define GLOC_BASE			(PERIPH_PBA_BASE + 0x4000*18)
+#define ABDACB_BASE			(PERIPH_PBA_BASE + 0x4000*19)
+#define TRNG_BASE			(PERIPH_PBA_BASE + 0x4000*20)
+#define PARC_BASE			(PERIPH_PBA_BASE + 0x4000*21)
+#define CATB_BASE			(PERIPH_PBA_BASE + 0x4000*22)
+/* (PERIPH_PBA_BASE + 0x4000*23) RESERVED */
+#define TWIM2_BASE			(PERIPH_PBA_BASE + 0x4000*24)
+#define TWIM3_BASE			(PERIPH_PBA_BASE + 0x4000*25)
+#define LCDCA_BASE			(PERIPH_PBA_BASE + 0x4000*26)
+
+/* --- SAM4L peripheral bridge B -------------------------------------------- */
+#define PERIPH_PBB_BASE			(PERIPH_BASE + 0xA0000U)
+#define PICOCACHE_BASE			(PERIPH_PBB_BASE + 0x400)
+#define FLASHCALW_BASE			(PERIPH_PBB_BASE + 0x1000*0)
+#define HMATRIX_BASE			(PERIPH_PBB_BASE + 0x1000*1)
+#define PDCA_BASE			(PERIPH_PBB_BASE + 0x1000*2)
+#define SMAP_BASE			(PERIPH_PBB_BASE + 0x1000*3)
+#define CRCCU_BASE			(PERIPH_PBB_BASE + 0x1000*4)
+#define USBC_BASE			(PERIPH_PBB_BASE + 0x1000*5)
+#define PEVC_BASE			(PERIPH_PBB_BASE + 0x1000*6)
+
+/* --------- AESA ------------- */
+#define AESA_BASE			(PERIPH_BASE + 0xB0000)
+
+/* --- SAM4L peripheral bridge C -------------------------------------------- */
+#define PERIPH_PBC_BASE			(PERIPH_BASE + 0xE0000)
+#define PM_BASE			(PERIPH_PBC_BASE)
+#define CHIPID_BASE			(PERIPH_PBC_BASE + 0x740)
+#define SCIF_BASE			(PERIPH_PBC_BASE + 0x800)
+#define FREQM_BASE			(PERIPH_PBC_BASE + 0xC00)
+#define GPIO_BASE			(PERIPH_PBC_BASE + 0x1000)
+
+/* --- SAM4L peripheral bridge D -------------------------------------------- */
+#define PERIPH_PBD_BASE			(PERIPH_BASE + 0xF0000U)
+#define BPM_BASE			(PERIPH_PBD_BASE + 0x400*0)
+#define BSCIF_BASE			(PERIPH_PBD_BASE + 0x400*1)
+#define AST_BASE			(PERIPH_PBD_BASE + 0x400*2)
+#define WDT_BASE			(PERIPH_PBD_BASE + 0x400*3)
+#define EIC_BASE			(PERIPH_PBD_BASE + 0x400*4)
+#define PICOUART_BASE			(PERIPH_PBD_BASE + 0x400*5)
 
 #endif
