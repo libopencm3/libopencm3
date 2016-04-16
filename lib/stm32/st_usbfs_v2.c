fb06d17d30e82efeb9cac565775394d8d1c8288c
fenugrec <fenugrec@users.sourceforge.net>
2016-04-15 21:47:48 -0400

    stm32: usb (M0/M0+): allow unaligned buffers for st_usbfs_copy_from_pm()
    Fixes GH issues #401 , #461 . The previous implementation of copy_from_pm assumed the destination buffer was aligned on a 16-bit boundary. On M0/M0+ cores (stm32F0*, stm32L0*) this causes a hard fault.
    This implementation is from Kuldeep Dhaka's tree; it does a 16-bit copy only if the destination buffer is aligned, otherwise a bytewise copy.

--- lib/stm32/st_usbfs_v2.c
+++ lib/stm32/st_usbfs_v2.c
@@ -73,21 +73,29 @@ void st_usbfs_copy_to_pm(volatile void *vPM, const void *buf, uint16_t len)
 /**
  * Copy a data buffer from packet memory.
  *
- * @param buf Source pointer to data buffer.
- * @param vPM Destination pointer into packet memory.
+ * @param buf Destination pointer for data buffer.
+ * @param vPM Source pointer into packet memory.
  * @param len Number of bytes to copy.
  */
 void st_usbfs_copy_from_pm(void *buf, const volatile void *vPM, uint16_t len)
 {
-	uint16_t *lbuf = buf;
 	const volatile uint16_t *PM = vPM;
 	uint8_t odd = len & 1;
+	len >>= 1;
 
-	for (len >>= 1; len; PM++, lbuf++, len--) {
-		*lbuf = *PM;
+	if (((uintptr_t) buf) & 0x01) {
+		for (; len; PM++, len--) {
+			uint16_t value = *PM;
+			*(uint8_t *) buf++ = value;
+			*(uint8_t *) buf++ = value >> 8;
+		}
+	} else {
+		for (; len; PM++, buf += 2, len--) {
+			*(uint16_t *) buf = *PM;
+		}
 	}
 
 	if (odd) {
-		*(uint8_t *) lbuf = *(uint8_t *) PM;
+		*(uint8_t *) buf = *(uint8_t *) PM;
 	}
 }
