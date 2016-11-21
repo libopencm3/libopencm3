libopencm3/libopencm3

commit 422d708384c76b8bf3a860bdd084e3010e28afa0

usb: dwc_otg: enable clocks automatically.
ST usbfs-v1, v2, EFM32 USB, lm4f usb all automatically enable the clock
in their _init routine.  Do the same for the dwc_otg drivers to be
consistent.

lib/usb/usb_f107.c
@@ -52,6 +52,7 @@ const struct _usbd_driver stm32f107_usb_driver = {
 /** Initialize the USB device controller hardware of the STM32. */
 static usbd_device *stm32f107_usbd_init(void)
 {
+	rcc_periph_clock_enable(RCC_OTGFS);
 	OTG_FS_GINTSTS = OTG_GINTSTS_MMIS;
 
 	OTG_FS_GUSBCFG |= OTG_GUSBCFG_PHYSEL;

lib/usb/usb_f207.c
@@ -52,6 +52,7 @@ const struct _usbd_driver stm32f207_usb_driver = {
 /** Initialize the USB device controller hardware of the STM32. */
 static usbd_device *stm32f207_usbd_init(void)
 {
+	rcc_periph_clock_enable(RCC_OTGHS);
 	OTG_HS_GINTSTS = OTG_GINTSTS_MMIS;
 
 	OTG_HS_GUSBCFG |= OTG_GUSBCFG_PHYSEL;

commit 825b51a6a8e7cf6ebef9477879a43ca7d89a313b

usb: dwc_otg: fix missing includes
Went awol merging something :(
(Yes, dwc_otg is theoretically shared with efm and co, but at present
it's still pretty much the stm32 version)

Fixes: 422d708384c76b8bf3a860bdd084e3010e28afa0

lib/usb/usb_f107.c
@@ -21,6 +21,7 @@
 #include <libopencm3/cm3/common.h>
 #include <libopencm3/stm32/tools.h>
 #include <libopencm3/stm32/otg_fs.h>
+#include <libopencm3/stm32/rcc.h>
 #include <libopencm3/usb/usbd.h>
 #include "usb_private.h"
 #include "usb_fx07_common.h"

lib/usb/usb_f207.c
@@ -21,6 +21,7 @@
 #include <libopencm3/cm3/common.h>
 #include <libopencm3/stm32/tools.h>
 #include <libopencm3/stm32/otg_hs.h>
+#include <libopencm3/stm32/rcc.h>
 #include <libopencm3/usb/usbd.h>
 #include "usb_private.h"
 #include "usb_fx07_common.h"