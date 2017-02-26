62b58555e29daff7c7206ed8be74fcdc57e598e1
Matthew Lai <m@matthewlai.ca>
2017-02-26 20:47:02 +0000

    stm32: rcc: added missing ifdef for STM32F7 in rcc

--- include/libopencm3/stm32/rcc.h
+++ include/libopencm3/stm32/rcc.h
@@ -30,6 +30,8 @@
 #       include <libopencm3/stm32/f3/rcc.h>
 #elif defined(STM32F4)
 #       include <libopencm3/stm32/f4/rcc.h>
+#elif defined(STM32F7)
+#       include <libopencm3/stm32/f7/rcc.h>
 #elif defined(STM32L0)
 #       include <libopencm3/stm32/l0/rcc.h>
 #elif defined(STM32L1)
