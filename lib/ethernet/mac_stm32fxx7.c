329b611e4f6d897362e1265955e4c617d48af5ec
Gregory Schlomoff <gregs@eatsa.com>
2017-04-10 11:04:38 -0700

    stm32: ethernet, flash: tagging some function arguments as const

--- lib/ethernet/mac_stm32fxx7.c
+++ lib/ethernet/mac_stm32fxx7.c
@@ -47,7 +47,7 @@ uint32_t RxBD;
  *
  * @param[in] mac uint8_t* Desired MAC
  */
-void eth_set_mac(uint8_t *mac)
+void eth_set_mac(const uint8_t *mac)
 {
 	ETH_MACAHR(0) = ((uint32_t)mac[5] << 8) | (uint32_t)mac[4] |
 			ETH_MACA0HR_MACA0H;
