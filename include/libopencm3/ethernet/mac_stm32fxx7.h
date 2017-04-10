0e580137a11cafc87589ccc33889548d67ccdf92
Gregory Schlomoff <gregs@eatsa.com>
2017-04-10 11:20:39 -0700

    Custom scatter-gather for ethernet transmit to avoid extra copying when payload isn't contiguous.

--- include/libopencm3/ethernet/mac_stm32fxx7.h
+++ include/libopencm3/ethernet/mac_stm32fxx7.h
@@ -717,6 +717,7 @@ void eth_set_mac(const uint8_t *mac);
 void eth_desc_init(uint8_t *buf, uint32_t nTx, uint32_t nRx, uint32_t cTx,
 		    uint32_t cRx, bool isext);
 bool eth_tx(uint8_t *ppkt, uint32_t n);
+bool eth_tx_custom_sg(uint32_t (*copy_fn)(void* dst, void*ctx), void* ctx);
 bool eth_rx(uint8_t *ppkt, uint32_t *len, uint32_t maxlen);
 
 void eth_init(uint8_t phy, enum eth_clk clock);
