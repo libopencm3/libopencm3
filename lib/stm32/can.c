libopencm3/libopencm3

commit d9615a2eb72894c1684ab47bb306c78a220bc0b0

stm32: can: update to modern includes and apis
No functional change.  But allows automatic functionality on platforms
that support CAN with only makefile changes now.

lib/stm32/can.c
@@ -36,18 +36,7 @@ LGPL License Terms @ref lgpl_license
  */
 
 #include <libopencm3/stm32/can.h>
-
-#if defined(STM32F1)
-#	include <libopencm3/stm32/f1/rcc.h>
-#elif defined(STM32F2)
-#	include <libopencm3/stm32/f2/rcc.h>
-#elif defined(STM32F3)
-#	include <libopencm3/stm32/f3/rcc.h>
-#elif defined(STM32F4)
-#	include <libopencm3/stm32/f4/rcc.h>
-#else
-#	error "stm32 family not defined."
-#endif
+#include <libopencm3/stm32/rcc.h>
 
 /* Timeout for CAN INIT acknowledge
  * this value is difficult to define.
@@ -74,12 +63,10 @@ can_reg_base.
 void can_reset(uint32_t canport)
 {
 	if (canport == CAN1) {
-		rcc_peripheral_reset(&RCC_APB1RSTR, RCC_APB1RSTR_CAN1RST);
-		rcc_peripheral_clear_reset(&RCC_APB1RSTR, RCC_APB1RSTR_CAN1RST);
+		rcc_periph_reset_pulse(RCC_CAN1);
 	} else {
 #if defined(BX_CAN2_BASE)
-		rcc_peripheral_reset(&RCC_APB1RSTR, RCC_APB1RSTR_CAN2RST);
-		rcc_peripheral_clear_reset(&RCC_APB1RSTR, RCC_APB1RSTR_CAN2RST);
+		rcc_periph_reset_pulse(RCC_CAN2);
 #endif
 	}
 }

commit b0e050d10d12c42be031c34822117cfd3c5a0ea7

stm32: can: fix can_reset()
Clearly staging branch testers weren't testing.
Also, clearly the separation of RST_ bits and RCC_ bits is a complicated
annoyance.

Fixes: d9615a2eb728 update to modern include apis

lib/stm32/can.c
@@ -63,10 +63,10 @@ can_reg_base.
 void can_reset(uint32_t canport)
 {
 	if (canport == CAN1) {
-		rcc_periph_reset_pulse(RCC_CAN1);
+		rcc_periph_reset_pulse(RST_CAN1);
 	} else {
 #if defined(BX_CAN2_BASE)
-		rcc_periph_reset_pulse(RCC_CAN2);
+		rcc_periph_reset_pulse(RST_CAN2);
 #endif
 	}
 }