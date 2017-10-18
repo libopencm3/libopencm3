libopencm3/libopencm3

commit c6743f9ecdb816c5c907111a3a20fd430c70fdb1

vector: make common linker definitions available to users
_data_loadaddr, _data, _edata, _ebss, _stack and vector_table all now
available in vector.h.

Suggested on IRC

include/libopencm3/cm3/vector.h
@@ -61,4 +61,8 @@ typedef struct {
 	vector_table_entry_t irq[NVIC_IRQ_COUNT];
 } vector_table_t;
 
+/* Common symbols exported by the linker script(s): */
+extern unsigned _data_loadaddr, _data, _edata, _ebss, _stack;
+extern unsigned vector_table;
+
 #endif

lib/cm3/vector.c
@@ -26,8 +26,7 @@
 /* load the weak symbols for IRQ_HANDLERS */
 #include "../dispatch/vector_nvic.c"
 
-/* Symbols exported by the linker script(s): */
-extern unsigned _data_loadaddr, _data, _edata, _ebss, _stack;
+/* Less common symbols exported by the linker script(s): */
 typedef void (*funcp_t) (void);
 extern funcp_t __preinit_array_start, __preinit_array_end;
 extern funcp_t __init_array_start, __init_array_end;

commit 38125e9941ad9dba0fdbcc762dc4bdcd59b7c8c4

vector: fix externs
Improper/lazy build testing.

Fixes: c6743f9 vector: make common linker definitions available to users

include/libopencm3/cm3/vector.h
@@ -63,6 +63,6 @@ typedef struct {
 
 /* Common symbols exported by the linker script(s): */
 extern unsigned _data_loadaddr, _data, _edata, _ebss, _stack;
-extern unsigned vector_table;
+extern vector_table_t vector_table;
 
 #endif

lib/vf6xx/vector_chipset.c
@@ -19,8 +19,6 @@
 
 #include <libopencm3/cm3/scb.h>
 
-extern vector_table_t vector_table;
-
 static inline void pre_main(void)
 {
 	/*