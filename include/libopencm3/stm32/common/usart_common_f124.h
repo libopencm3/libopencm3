9b4c663fe200a50a4b61b079664cca2aa31cf8f5
Karl Palsson <karlp@tweak.net.au>
2017-10-25 22:14:30 +0000

    stm32: usart: move USARTX definitions to target/version specific
    Instead of declaring that _every_ device has USART1,2,3 and UART4,5, let
    the targets themselves define what periphs they do, along with their
    USARTx_BASE defines, and let the common headers just have the common
    abstractions.

--- include/libopencm3/stm32/common/usart_common_f124.h
+++ include/libopencm3/stm32/common/usart_common_f124.h
@@ -37,6 +37,19 @@ specific memorymap.h header before including this header file.*/
 
 #include <libopencm3/stm32/common/usart_common_all.h>
 
+
+/** @defgroup usart_reg_base USART register base addresses
+ * @ingroup STM32F_usart_defines
+ * Holds all the U(S)ART peripherals supported.
+ * @{
+ */
+#define USART1				USART1_BASE
+#define USART2				USART2_BASE
+#define USART3				USART3_BASE
+#define UART4				UART4_BASE
+#define UART5				UART5_BASE
+/**@}*/
+
 /* --- USART registers ----------------------------------------------------- */
 
 /* Status register (USARTx_SR) */
