580a2a4a63cad6ca32ac837cf6d1c64664ce80f3
Yonghua Zheng <yh.zheng@yuneec.com>
2017-12-04 10:51:29 +0800

    stm32f7: usart: enable usart peripheral
    Add usart-v2 to stm32f7 to provide usart support in f7 series.

--- include/libopencm3/stm32/usart.h
+++ include/libopencm3/stm32/usart.h
@@ -30,6 +30,8 @@
 #       include <libopencm3/stm32/f3/usart.h>
 #elif defined(STM32F4)
 #       include <libopencm3/stm32/f4/usart.h>
+#elif defined(STM32F7)
+#       include <libopencm3/stm32/f7/usart.h>
 #elif defined(STM32L0)
 #       include <libopencm3/stm32/l0/usart.h>
 #elif defined(STM32L1)
