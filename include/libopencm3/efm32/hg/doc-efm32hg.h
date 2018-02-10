8787055f71e33ffc7db78d1dfdf4ec2f203d9b23
Sebastian Holzapfel <seb.holzapfel@data61.csiro.au>
2018-02-11 09:16:05 +1100

    efm32hg: add basic makefile, interrupts, device information

---
+++ include/libopencm3/efm32/hg/doc-efm32hg.h
@@ -0,0 +1,32 @@
+/** @mainpage libopencm3 EFM32 Happy Gecko
+
+@version 1.0.0
+
+@date 28 January 2018
+
+API documentation for Energy Micro EFM32 Happy Gecko Cortex M0+ series.
+
+LGPL License Terms @ref lgpl_license
+*/
+
+/** @defgroup EFM32LG EFM32 HappyGecko
+Libraries for Energy Micro EFM32 Happy Gecko series.
+
+@version 1.0.0
+
+@date 28 January 2018
+
+LGPL License Terms @ref lgpl_license
+*/
+
+/** @defgroup EFM32HG_defines EFM32 Happy Gecko Defines
+
+@brief Defined Constants and Types for the Energy Micro EFM32 Happy Gecko
+series
+
+@version 1.0.0
+
+@date 28 January 2018
+
+LGPL License Terms @ref lgpl_license
+*/
