a1264f5065bd8fd799b302c6d55260ef68765779
Christian Tacke <Christian.Tacke+libopencm3@cosmokey.com>
2018-02-26 13:46:27 +0100

    stm32l4: usart: Fix USART3 definition/typo
    USART*3* should point to *3* not *2*.

--- include/libopencm3/stm32/l4/usart.h
+++ include/libopencm3/stm32/l4/usart.h
@@ -37,7 +37,7 @@
  */
 #define USART1				USART1_BASE
 #define USART2				USART2_BASE
-#define USART3				USART2_BASE
+#define USART3				USART3_BASE
 #define UART4				USART4_BASE
 #define UART5				USART5_BASE
 #define LPUART1				LPUART1_BASE
