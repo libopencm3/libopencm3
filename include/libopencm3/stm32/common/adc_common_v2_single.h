34462ac54ae6c7e3dbecdd34096f268ea8b879e9
Karl Palsson <karlp@tweak.net.au>
2018-04-04 19:21:29 +0000

    stm32: adc v2: add some minor doxygen

--- include/libopencm3/stm32/common/adc_common_v2_single.h
+++ include/libopencm3/stm32/common/adc_common_v2_single.h
@@ -43,12 +43,14 @@ specific memorymap.h header before including this header file.*/
 #define LIBOPENCM3_ADC_COMMON_V2_SINGLE_H
 
 /* ----- ADC registers  -----------------------------------------------------*/
-/* Channel Select Register */
+/** Channel Select Register */
 #define ADC_CHSELR(adc)		MMIO32((adc) + 0x28)
 
 /* ----- ADC registers values -----------------------------------------------*/
 /* ADC_CFGR1 values */
+/** Wait conversion mode */
 #define ADC_CFGR1_WAIT		(1<<14)
+/** Auto off mode */
 #define ADC_CFGR1_AUTOFF		(1 << 15)
 
 /* EXTSEL[2:0]: External trigger selection for regular group */
