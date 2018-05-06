9ddafa6284e9dac1e3c8787475655a5381f57bfc
Karl Palsson <karlp@tweak.net.au>
2018-05-06 22:16:55 +0000

    stm32f7: enable common timers support

--- include/libopencm3/stm32/timer.h
+++ include/libopencm3/stm32/timer.h
@@ -32,6 +32,8 @@
 #       include <libopencm3/stm32/f3/timer.h>
 #elif defined(STM32F4)
 #       include <libopencm3/stm32/f4/timer.h>
+#elif defined(STM32F7)
+#       include <libopencm3/stm32/f7/timer.h>
 #elif defined(STM32L0)
 #       include <libopencm3/stm32/l0/timer.h>
 #elif defined(STM32L1)
