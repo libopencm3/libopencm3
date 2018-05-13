6692fc51aca2ee543f45b0a013e7f3d8584b743e
ziggi <borodin@unix7.org>
2018-05-13 10:16:45 +0200

    Added STM32 FSMC code

--- include/libopencm3/stm32/fsmc.h
+++ include/libopencm3/stm32/fsmc.h
@@ -305,4 +305,33 @@
 /* ECCx[31:0]: ECC result */
 #define FSMC_ECCR_ECCX			(1 << 0)
 
+
+void fsmc_write_burst_disable(uint32_t bcr);
+void fsmc_write_burst_enable(uint32_t bcr);
+void fsmc_extended_mode_disable(uint32_t bcr);
+void fsmc_extended_mode_enable(uint32_t bcr);
+void fsmc_write_disable(uint32_t bcr);
+void fsmc_write_enable(uint32_t bcr);
+void fsmc_wait_disable(uint32_t bcr);
+void fsmc_wait_enable(uint32_t bcr);
+void fsmc_wait_signal_polarity_low(uint32_t bcr);
+void fsmc_wait_signal_polarity_high(uint32_t bcr);
+void fsmc_wrapped_burst_mode_disable(uint32_t bcr);
+void fsmc_wrapped_burst_mode_enable(uint32_t bcr);
+void fsmc_burst_disable(uint32_t bcr);
+void fsmc_burst_enable(uint32_t bcr);
+void fsmc_wait_timing_configuration_disable(uint32_t bcr);
+void fsmc_wait_timing_configuration_enable(uint32_t bcr);
+void fsmc_address_data_multiplexing_disable(uint32_t bcr);
+void fsmc_address_data_multiplexing_enable(uint32_t bcr);
+void fsmc_memory_bank_enable(uint32_t bcr);
+void fsmc_memory_bank_disable(uint32_t bcr);
+void fsmc_set_access_mode(uint32_t bank, uint32_t mode);
+void fsmc_set_data_latency(uint32_t bank, uint32_t lat);
+void fsmc_set_clock_divide_ratio(uint32_t bank, uint32_t div);
+void fsmc_set_turnaround_phase_duration(uint32_t bank, uint32_t duration);
+void fsmc_set_data_phase_duration(uint32_t bank, uint32_t duration);
+void fsmc_set_address_hold_phase_duration(uint32_t bank, uint32_t duration);
+void fsmc_set_address_setup_phase_duration(uint32_t bank, uint32_t duration);
+
 #endif
