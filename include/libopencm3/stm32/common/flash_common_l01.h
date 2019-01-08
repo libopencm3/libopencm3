43bf140b3a45669046e9d513eeb2455b7dac65dd
Guillaume Revaillot <revaillot@archos.com>
2019-01-08 14:26:38 +0100

    stm32l[01]: flash common: add flash_unlock_acr, allowing to unlock FLASH_ACR RUN_PD bit.
    flash_unlock_acr allows to unlock RUN_PD bit from FLASH_ACR register. Relock is done automatically
    when writing 0 to RUN_PD, so no flash_lock_acr method.

--- include/libopencm3/stm32/common/flash_common_l01.h
+++ include/libopencm3/stm32/common/flash_common_l01.h
@@ -120,6 +120,8 @@ void flash_unlock_progmem(void);
 void flash_lock_progmem(void);
 void flash_lock_option_bytes(void);
 
+void flash_unlock_acr(void);
+
 void eeprom_program_word(uint32_t address, uint32_t data);
 void eeprom_program_words(uint32_t address, uint32_t *data, int length_in_words);
 
