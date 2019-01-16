f13a9eee5b16f8b34d22744730fead6bdd05b522
Guillaume Revaillot <revaillot@archos.com>
2019-01-16 17:41:42 +0100

    stm32g0: add power.
    neither v1 nor v2...

--- include/libopencm3/stm32/pwr.h
+++ include/libopencm3/stm32/pwr.h
@@ -38,6 +38,8 @@
 #       include <libopencm3/stm32/l0/pwr.h>
 #elif defined(STM32L4)
 #       include <libopencm3/stm32/l4/pwr.h>
+#elif defined(STM32G0)
+#       include <libopencm3/stm32/g0/pwr.h>
 #else
 #       error "stm32 family not defined."
 #endif
