bb98d0755cc64298c781c45c95607c4f0fd49dc5
Guillaume Revaillot <revaillot@archos.com>
2019-01-17 18:10:47 +0100

    stm32g0: add iwdg.
    regular v2 iwdg.

--- include/libopencm3/stm32/iwdg.h
+++ include/libopencm3/stm32/iwdg.h
@@ -38,6 +38,8 @@
 #       include <libopencm3/stm32/l1/iwdg.h>
 #elif defined(STM32L4)
 #       include <libopencm3/stm32/l4/iwdg.h>
+#elif defined(STM32G0)
+#       include <libopencm3/stm32/g0/iwdg.h>
 #else
 #       error "stm32 family not defined."
 #endif
