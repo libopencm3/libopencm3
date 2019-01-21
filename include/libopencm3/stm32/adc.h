38b45c87861e5461283609f3aa2df31b141faae6
Guillaume Revaillot <revaillot@archos.com>
2019-01-21 15:36:49 +0100

    stm32g0: add adc.
    v2 "single" peripheral with a couple of tweaks :
     - added registers to configure two additionnal advanced analog watchdog.
     - different adc sampling time time based on channel groups.
     - 8 steps adc sequence injection, using chselr/chselrmode.
    
    And a note on the rm explaining that after every configuration change to ADC_CFGR1's
    SCANDIR or CHSELRMOD or CHSELR register, user need to check that configuration
    is applied before any other modification / adc conversion start.. making adc_set_reqular
    a bit painfull to read..

--- include/libopencm3/stm32/adc.h
+++ include/libopencm3/stm32/adc.h
@@ -36,6 +36,8 @@
 #       include <libopencm3/stm32/l1/adc.h>
 #elif defined(STM32L4)
 #       include <libopencm3/stm32/l4/adc.h>
+#elif defined(STM32G0)
+#       include <libopencm3/stm32/g0/adc.h>
 #else
 #       error "stm32 family not defined."
 #endif
