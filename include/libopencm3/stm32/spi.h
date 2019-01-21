ba3b50a4ad68c3c94f1d1c45a97e7fbd32f6a4cf
Guillaume Revaillot <revaillot@archos.com>
2019-01-21 12:07:43 +0100

    stm32g0: add spi.
    classic "common" spi + frf bit spi peripheral.
    As for i2c, i could only check signals on scope, no spi slave to check, but looks ok.

--- include/libopencm3/stm32/spi.h
+++ include/libopencm3/stm32/spi.h
@@ -38,6 +38,8 @@
 #       include <libopencm3/stm32/l1/spi.h>
 #elif defined(STM32L4)
 #       include <libopencm3/stm32/l4/spi.h>
+#elif defined(STM32G0)
+#       include <libopencm3/stm32/g0/spi.h>
 #else
 #       error "stm32 family not defined."
 #endif
