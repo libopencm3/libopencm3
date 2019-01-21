a51ecb4719995a3de117922d756e8b5634a2fa76
Guillaume Revaillot <revaillot@archos.com>
2019-01-21 15:20:46 +0100

    stm32g0: add usart.

--- include/libopencm3/stm32/usart.h
+++ include/libopencm3/stm32/usart.h
@@ -38,6 +38,8 @@
 #       include <libopencm3/stm32/l1/usart.h>
 #elif defined(STM32L4)
 #       include <libopencm3/stm32/l4/usart.h>
+#elif defined(STM32G0)
+#       include <libopencm3/stm32/g0/usart.h>
 #else
 #       error "stm32 family not defined."
 #endif
