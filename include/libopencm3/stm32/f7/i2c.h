cceb230e0b50634ad031d0a1628f359a8c614331
Ross Schlaikjer <ross@schlaikjer.net>
2019-04-04 17:06:48 -0400

    Remove old copyright line

--- include/libopencm3/stm32/f7/i2c.h
+++ include/libopencm3/stm32/f7/i2c.h
@@ -13,8 +13,6 @@
 /*
  * This file is part of the libopencm3 project.
  *
- * Copyright (C) 2010 Thomas Otto <tommi@viadmin.org>
- *
  * This library is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
