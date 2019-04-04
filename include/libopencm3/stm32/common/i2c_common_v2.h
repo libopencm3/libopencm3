4db40e08394fdc6742f9117f3098d23a5de2bd95
Ross Schlaikjer <ross@schlaikjer.net>
2019-04-04 11:06:03 -0400

    stm32f7: Include i2c_common_v2
    With the addition of a define for I2C4, the existing common i2c
    functions seem to work out of the box on the F7 (tested on an
    STM32F750).

--- include/libopencm3/stm32/common/i2c_common_v2.h
+++ include/libopencm3/stm32/common/i2c_common_v2.h
@@ -46,6 +46,9 @@ specific memorymap.h header before including this header file.*/
 #ifdef I2C3_BASE
 #define I2C3				I2C3_BASE
 #endif
+#ifdef I2C4_BASE
+#define I2C4				I2C4_BASE
+#endif
 /**@}*/
 
 /* --- I2C registers ------------------------------------------------------- */
