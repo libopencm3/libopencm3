1b032d4a8fc7508b36f84f72b272cfdce807a35d
vector <ettore.zaffaroni@gmail.com>
2019-05-08 18:24:57 +0200

    changed comments to doxygen comments in f4 and f7 dac.h

--- include/libopencm3/stm32/f4/dac.h
+++ include/libopencm3/stm32/f4/dac.h
@@ -34,18 +34,18 @@ LGPL License Terms @ref lgpl_license
 #include <libopencm3/stm32/common/dac_common_all.h>
 
 
-/* DAC status register (DAC_SR) */
-/* not available on STM32F4x1/STM32F4x2 */
+/** DAC status register (DAC_SR).
+ * not available on STM32F4x1/STM32F4x2 */
 #define DAC_SR				MMIO32(DAC_BASE + 0x34)
 
 /* --- DAC_SR values ------------------------------------------------------- */
 
-/* DMAUDR2: DAC channel 1 DMA underrun flag */
+/** DMAUDR2: DAC channel 1 DMA underrun flag */
 #define DAC_SR_DMAUDR1		(1 << 13)
 
 
-/* DMAUDR2: DAC channel 2 DMA underrun flag */
-/* not available on STM32F4x1/STM32F4x2 and STM32F410*/
+/** DMAUDR2: DAC channel 2 DMA underrun flag.
+ * not available on STM32F4x1/STM32F4x2 and STM32F410*/
 #define DAC_SR_DMAUDR2		(1 << 29)
 
 
