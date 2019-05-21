2bed518f09aa7223e604e8f0bb38f67196be6a94
Karl Palsson <karlp@tweak.net.au>
2019-05-21 23:45:34 +0000

    stm32f7: doc: flash defines weren't grouped at all

--- include/libopencm3/stm32/f7/flash.h
+++ include/libopencm3/stm32/f7/flash.h
@@ -1,7 +1,6 @@
-#ifndef LIBOPENCM3_FLASH_H
-#define LIBOPENCM3_FLASH_H
-
-/** @addtogroup flash_defines
+/** @defgroup flash_defines FLASH Defines
+ *
+ * @ingroup STM32F7xx_defines
  *
  * @author @htmlonly &copy; @endhtmlonly 2017
  * Matthew Lai <m@matthewlai.ca>
@@ -31,6 +30,9 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with this library.  If not, see <http://www.gnu.org/licenses/>.
  */
+
+#ifndef LIBOPENCM3_FLASH_H
+#define LIBOPENCM3_FLASH_H
 
 #include <libopencm3/stm32/common/flash_common_all.h>
 #include <libopencm3/stm32/common/flash_common_f.h>
