fe5e82a3ccb8250ab1f3b3013dcf1526538dc61d
Karl Palsson <karlp@tweak.net.au>
2019-05-21 22:13:43 +0000

    stm32: doc: dac: move to peripheral_apis.
    Drops all the stub dummy header files, many of which weren't even being
    built, and moves them to the clean peripherals_api doc section.

--- lib/stm32/common/dac_common_all.c
+++ lib/stm32/common/dac_common_all.c
@@ -1,12 +1,13 @@
-/** @addtogroup dac_file
+/** @addtogroup dac_file DAC peripheral API
+ * @ingroup peripheral_apis
 
 @author @htmlonly &copy; @endhtmlonly 2012 Ken Sarkies ksarkies@internode.on.net
 
 This library supports the Digital to Analog Conversion System in the
-STM32F series of ARM Cortex Microcontrollers by ST Microelectronics.
+STM32 series of ARM Cortex Microcontrollers by ST Microelectronics.
 
-The DAC is present only in a limited set of devices, notably some
-of the connection line, high density and XL devices.
+The DAC peripheral found on many of the devices in the STM32 lineup,
+sometimes with only one channel, but normally with two channels.
 
 Two DAC channels are available, however unlike the ADC channels these
 are separate DAC devices controlled by the same register block.
