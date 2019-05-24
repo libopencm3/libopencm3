5312aba8243c8d50afc8cb56841966678ad63f69
Ross Schlaikjer <ross@schlaikjer.net>
2019-05-24 12:00:02 -0400

    Add ITM utility methods

--- include/libopencm3/cm3/itm.h
+++ include/libopencm3/cm3/itm.h
@@ -20,6 +20,12 @@
 #ifndef LIBOPENCM3_CM3_ITM_H
 #define LIBOPENCM3_CM3_ITM_H
 
+#include <stdbool.h>
+#include <stdint.h>
+
+#include <libopencm3/cm3/common.h>
+#include <libopencm3/cm3/memorymap.h>
+
 /**
  * @defgroup cm_itm Cortex-M Instrumentation Trace Macrocell (ITM)
  * @ingroup CM3_defines
@@ -71,6 +77,9 @@
  *             1: Privileged access only to stimulus ports
  */
 
+/* --- ITM_LAR values ------------------------------------------------------ */
+#define ITM_LAR_WRITE_ENABLE (0xC5ACCE55)
+
 /* --- ITM_TCR values ------------------------------------------------------ */
 
 /* Bits 31:24 - Reserved */
@@ -88,6 +97,59 @@
 #define ITM_TCR_SYNCENA			(1 << 2)
 #define ITM_TCR_TSENA			(1 << 1)
 #define ITM_TCR_ITMENA			(1 << 0)
+
+/* --- ITM port bitmask values ---------------------------------------------- */
+#define ITM_PORT0  (1 << 0)
+#define ITM_PORT1  (1 << 1)
+#define ITM_PORT2  (1 << 2)
+#define ITM_PORT3  (1 << 3)
+#define ITM_PORT4  (1 << 4)
+#define ITM_PORT5  (1 << 5)
+#define ITM_PORT6  (1 << 6)
+#define ITM_PORT7  (1 << 7)
+#define ITM_PORT8  (1 << 8)
+#define ITM_PORT9  (1 << 9)
+#define ITM_PORT10 (1 << 10)
+#define ITM_PORT11 (1 << 11)
+#define ITM_PORT12 (1 << 12)
+#define ITM_PORT13 (1 << 13)
+#define ITM_PORT14 (1 << 14)
+#define ITM_PORT15 (1 << 15)
+#define ITM_PORT16 (1 << 16)
+#define ITM_PORT17 (1 << 17)
+#define ITM_PORT18 (1 << 18)
+#define ITM_PORT19 (1 << 19)
+#define ITM_PORT20 (1 << 20)
+#define ITM_PORT21 (1 << 21)
+#define ITM_PORT22 (1 << 22)
+#define ITM_PORT23 (1 << 23)
+#define ITM_PORT24 (1 << 24)
+#define ITM_PORT25 (1 << 25)
+#define ITM_PORT26 (1 << 26)
+#define ITM_PORT27 (1 << 27)
+#define ITM_PORT28 (1 << 28)
+#define ITM_PORT29 (1 << 29)
+#define ITM_PORT30 (1 << 30)
+#define ITM_PORT31 (1 << 31)
+
+/*****************************************************************************/
+/* API Functions                                                             */
+/*****************************************************************************/
+
+BEGIN_DECLS
+
+void itm_set_write_access_enabled(bool enable);
+void itm_set_trace_bus_id(uint8_t id);
+void itm_set_enabled(bool enable);
+void itm_enable_stimulus_ports(uint32_t ports);
+void itm_disable_stimulus_ports(uint32_t ports);
+void itm_set_stimulus_port_mask(uint32_t mask);
+void itm_send_blocking_u8(uint8_t port, uint8_t datum);
+void itm_send_blocking_u16(uint8_t port, uint16_t datum);
+void itm_send_blocking_u32(uint8_t port, uint32_t datum);
+void itm_send_string(uint8_t port, const char *s);
+
+END_DECLS
 
 /**@}*/
 
---
+++ lib/cm3/itm.c
@@ -0,0 +1,120 @@
+/*
+ * This file is part of the libopencm3 project.
+ *
+ * Copyright (C) 2019
+ *
+ * This library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <libopencm3/cm3/itm.h>
+
+/*---------------------------------------------------------------------------*/
+/** @brief ITM Enable/disable write access to ITM control registers
+ *
+ * In order to access control registers 0xE00:0xFFC, the Lock Access Register
+ * must be written with a magic value.
+ * Writing any other value will disable write access to the registers.
+ *
+ * @return void
+ */
+void itm_set_write_access_enabled(bool enable) {
+  if (enable) {
+    ITM_LAR = ITM_LAR_WRITE_ENABLE;
+  } else {
+    ITM_LAR = 0;
+  }
+}
+
+/*---------------------------------------------------------------------------*/
+/** @brief ITM Set Advanced Trace Bus ID
+ *
+ * @return void
+ */
+void itm_set_trace_bus_id(uint8_t id) {
+  ITM_TCR = (ITM_TCR & ~ITM_TCR_TRACE_BUS_ID_MASK) |
+            ((id & ITM_TCR_TRACE_BUS_ID_MASK) << 16);
+}
+
+/*---------------------------------------------------------------------------*/
+/** @brief ITM Enable/disable ITM.
+ *
+ * Set/clear the master enable bit on the ITM. This must be enabled before ITM
+ * stimulus and trace enable registers can be written.
+ *
+ * @return void
+ */
+void itm_set_enabled(bool enable) {
+  if (enable) {
+    ITM_TCR |= ITM_TCR_ITMENA;
+  } else {
+    ITM_TCR &= ~ITM_TCR_ITMENA;
+  }
+}
+
+/*---------------------------------------------------------------------------*/
+/** @brief Enable a given set of stimulus ports (ITM_PORTN) */
+void itm_enable_stimulus_ports(uint32_t ports) { ITM_TER[0] |= ports; }
+
+/*---------------------------------------------------------------------------*/
+/** @brief Disable a given set of stimulus ports (ITM_PORTN) */
+void itm_disable_stimulus_ports(uint32_t ports) { ITM_TER[0] &= ~ports; }
+
+/*---------------------------------------------------------------------------*/
+/** @brief Directly configure the mask of enabled ITM ports (ITM_PORTN) */
+void itm_set_stimulus_port_mask(uint32_t mask) { ITM_TER[0] = mask; }
+
+/*---------------------------------------------------------------------------*/
+/** @brief Write a byte to the given ITM stimulus port */
+void itm_send_blocking_u8(uint8_t port, uint8_t datum) {
+  // Wait for stimulus port to become available
+  while (!(ITM_STIM8(port) & ITM_STIM_FIFOREADY)) {
+  }
+  // Write datum
+  ITM_STIM8(port) = datum;
+}
+
+/*---------------------------------------------------------------------------*/
+/** @brief Write a byte to the given ITM stimulus port */
+void itm_send_blocking_u16(uint8_t port, uint16_t datum) {
+  // Wait for stimulus port to become available
+  while (!(ITM_STIM16(port) & ITM_STIM_FIFOREADY)) {
+  }
+  // Write datum
+  ITM_STIM16(port) = datum;
+}
+
+/*---------------------------------------------------------------------------*/
+/** @brief Write a byte to the given ITM stimulus port */
+void itm_send_blocking_u32(uint8_t port, uint32_t datum) {
+  // Wait for stimulus port to become available
+  while (!(ITM_STIM32(port) & ITM_STIM_FIFOREADY)) {
+  }
+  // Write datum
+  ITM_STIM32(port) = datum;
+}
+
+/*---------------------------------------------------------------------------*/
+/** @brief Write a C string to the given stimulus port
+ *
+ * String must be terminated with a NULL byte.
+ * This method will block until the data is fully sent.
+ */
+void itm_send_string(uint8_t port, const char *s) {
+  // While we do not hit a '\0', send a byte on the given ITM port
+  while (*s) {
+    itm_send_blocking_u8(port, ((uint8_t)*s));
+    s++;
+  }
+}
+
