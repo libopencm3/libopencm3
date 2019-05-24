libopencm3/libopencm3

commit 96d094af13dab67709a252a39da164bde4d637e0

mk/genlink-config: provide LIBNAME in devices autogeneration
This makes the generic rules workk happily regardless of whether the
linkerscript generation is being used or not.

Notably, in the examples repo, use of the linker script autogeneration
was retriggering library builds as LIBNAME was unset and therefore the
libraries lib<blank>.a couldn't be found.

mk/genlink-config.mk
@@ -55,16 +55,17 @@ endif
 # only append to LDFLAGS if the library file exists to not break builds
 # where those are provided by different means
 ifneq (,$(wildcard $(OPENCM3_DIR)/lib/libopencm3_$(genlink_family).a))
-LDLIBS += -lopencm3_$(genlink_family)
-LIBDEPS += $(OPENCM3_DIR)/lib/libopencm3_$(genlink_family).a
+LIBNAME = opencm3_$(genlink_family)
 else
 ifneq (,$(wildcard $(OPENCM3_DIR)/lib/libopencm3_$(genlink_subfamily).a))
-LDLIBS += -lopencm3_$(genlink_subfamily)
-LIBDEPS += $(OPENCM3_DIR)/lib/libopencm3_$(genlink_subfamily).a
+LIBNAME = opencm3_$(genlink_subfamily)
 else
 $(warning $(OPENCM3_DIR)/lib/libopencm3_$(genlink_family).a library variant for the selected device does not exist.)
 endif
 endif
+
+LDLIBS += -l$(LIBNAME)
+LIBDEPS += $(OPENCM3_DIR)/lib/lib$(LIBNAME)
 
 # only append to LDLIBS if the directory exists
 ifneq (,$(wildcard $(OPENCM3_DIR)/lib))

commit 6ccb2d0a8312141952b3cadc5cb3f7ca8bf1d960

Add .a suffix to LIBDEPS var in genlink-config
It looks like commit 96d094af13dab67709a252a39da164bde4d637e0
introduced a build failure to the gadget for the tilm4f120xl target,
which appears to be the only one to use genlink.

Adding back the `.a` suffix to the rule appears to fix the problem.

mk/genlink-config.mk
@@ -65,7 +65,7 @@ endif
 endif
 
 LDLIBS += -l$(LIBNAME)
-LIBDEPS += $(OPENCM3_DIR)/lib/lib$(LIBNAME)
+LIBDEPS += $(OPENCM3_DIR)/lib/lib$(LIBNAME).a
 
 # only append to LDLIBS if the directory exists
 ifneq (,$(wildcard $(OPENCM3_DIR)/lib))