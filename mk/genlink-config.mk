libopencm3/libopencm3

commit 96d094af13dab67709a252a39da164bde4d637e0

mk/genlink-config: provide LIBNAME in devices autogeneration
This makes the generic rules workk happily regardless of whether the
linkerscript generation is being used or not.

Notably, in the examples repo, use of the linker script autogeneration
was retriggering library builds as LIBNAME was unset and therefore the
libraries lib<blank>.a couldn't be found.

mk/genlink-config.mk
@@ -55,16 +55,17 @@ endif
 # only append to LDFLAGS if the library file exists to not break builds
 # where those are provided by different means
 ifneq (,$(wildcard $(OPENCM3_DIR)/lib/libopencm3_$(genlink_family).a))
-LDLIBS += -lopencm3_$(genlink_family)
-LIBDEPS += $(OPENCM3_DIR)/lib/libopencm3_$(genlink_family).a
+LIBNAME = opencm3_$(genlink_family)
 else
 ifneq (,$(wildcard $(OPENCM3_DIR)/lib/libopencm3_$(genlink_subfamily).a))
-LDLIBS += -lopencm3_$(genlink_subfamily)
-LIBDEPS += $(OPENCM3_DIR)/lib/libopencm3_$(genlink_subfamily).a
+LIBNAME = opencm3_$(genlink_subfamily)
 else
 $(warning $(OPENCM3_DIR)/lib/libopencm3_$(genlink_family).a library variant for the selected device does not exist.)
 endif
 endif
+
+LDLIBS += -l$(LIBNAME)
+LIBDEPS += $(OPENCM3_DIR)/lib/lib$(LIBNAME)
 
 # only append to LDLIBS if the directory exists
 ifneq (,$(wildcard $(OPENCM3_DIR)/lib))

commit f700781975dc823e8d02288aacbc775c6effed3f

genlinkk-config: fix missing .a suffix
Fixes: 96d094af13 mk/genlink-config: provide LIBNAME in devices autogeneration

The initial fix had fixed the examples repo, but broke the tests and the
template repo.  Restore the suffix properly.

mk/genlink-config.mk
@@ -65,7 +65,7 @@ endif
 endif
 
 LDLIBS += -l$(LIBNAME)
-LIBDEPS += $(OPENCM3_DIR)/lib/lib$(LIBNAME)
+LIBDEPS += $(OPENCM3_DIR)/lib/lib$(LIBNAME).a
 
 # only append to LDLIBS if the directory exists
 ifneq (,$(wildcard $(OPENCM3_DIR)/lib))