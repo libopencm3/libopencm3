a143e5644e1c94d2e7fc4626b2e9fb2469aa37e7
Karl Palsson <karlp@etactica.com>
2019-06-11 21:42:55 +0000

    doc: stm32f1:rtc: fix trivial param typos

--- lib/stm32/f1/rtc.c
+++ lib/stm32/f1/rtc.c
@@ -241,7 +241,7 @@ uint32_t rtc_get_alarm_val(void)
 /*---------------------------------------------------------------------------*/
 /** @brief RTC set the Counter
 
-@param[in] uint32_t counter_val: 32 bit time setting for the counter.
+@param[in] counter_val 32 bit time setting for the counter.
 */
 
 void rtc_set_counter_val(uint32_t counter_val)
