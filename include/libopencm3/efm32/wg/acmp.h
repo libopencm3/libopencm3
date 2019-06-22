7e2cd050aa1c85e4a60c244dedf32265fb8b4da2
Karl Palsson <karlp@tweak.net.au>
2019-06-22 01:36:06 +0000

    doc: efm32: acmp: document, and include via peripheral_apis
    Requires a stub .c file as it has common includes with out any common
    code (yet)

--- include/libopencm3/efm32/wg/acmp.h
+++ include/libopencm3/efm32/wg/acmp.h
@@ -1,3 +1,11 @@
+/** @defgroup acmp_defines ACMP Defines
+ *
+ * @brief <b>Defined Constants and Types for the Analog Comparator module</b>
+ *
+ * @ingroup EFM32WG_defines
+ *
+ * LGPL License Terms @ref lgpl_license
+ */
 /*
  * This file is part of the libopencm3 project.
  *
@@ -17,9 +25,6 @@
  * along with this library.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#ifndef LIBOPENCM3_EFM32_WG_ACMP_H
-#define LIBOPENCM3_EFM32_WG_ACMP_H
+#pragma once
 
 #include <libopencm3/efm32/common/acmp_common.h>
-
-#endif
