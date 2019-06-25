2d1277e1fae9552d11499a1573ecad39d0147d99
Karl Palsson <karlp@etactica.com>
2019-06-25 12:06:47 +0000

    doc: efm32: uart/usart provide common doxygen
    Fix some direct includes, tag properly, include a stub file to pull in
    shared header documentation and re-arrange some existing documentation
    to make it present nicely.

--- include/libopencm3/efm32/lg/uart.h
+++ include/libopencm3/efm32/lg/uart.h
@@ -1,3 +1,11 @@
+/** @defgroup uart_defines UART Defines
+ *
+ * @brief <b>Defined Constants and Types for the UART module</b>
+ *
+ * @ingroup EFM32LG_defines
+ *
+ * LGPL License Terms @ref lgpl_license
+ */
 /*
  * This file is part of the libopencm3 project.
  *
@@ -17,9 +25,6 @@
  * along with this library.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#ifndef LIBOPENCM3_EFM32_LG_UART_H
-#define LIBOPENCM3_EFM32_LG_UART_H
+#pragma once
 
 #include <libopencm3/efm32/common/uart_common.h>
-
-#endif
---
+++ lib/efm32/common/usart_common.c
@@ -0,0 +1,18 @@
+/** @addtogroup usart_file UART/USART peripheral API
+ * @ingroup peripheral_apis
+ * @brief UART/USART helper functions.
+ *
+ * <b>NO</b> helper functions exist. Only header definitions are available.
+ * Delete these lines if/when you add actual helper APIs.
+ * @sa usart_defines
+ * @sa uart_defines
+ * @copyright See @ref lgpl_license
+ */
+
+#include <libopencm3/efm32/uart.h>
+#include <libopencm3/efm32/usart.h>
+
+/**@{*/
+
+/**@}*/
+
