9b3ab933acfb067fa5f6d8d2c7b18004b463f737
Karl Palsson <karlp@etactica.com>
2019-06-25 11:35:54 +0000

    doc: efm32: wdog: include stub file
    Tags were added, but without the stub file, common headers aren't picked
    up by the auto source list generation

---
+++ lib/efm32/common/wdog_common.c
@@ -0,0 +1,15 @@
+/** @addtogroup wdog_file WDOG peripheral API
+ * @ingroup peripheral_apis
+ * @brief Watchdog Module helper functions.
+ *
+ * <b>NO</b> helper functions exist. Only header definitions are available.
+ * Delete these lines if/when you add actual helper APIs.
+ * @copyright See @ref lgpl_license
+ */
+
+#include <libopencm3/efm32/wdog.h>
+
+/**@{*/
+
+/**@}*/
+
