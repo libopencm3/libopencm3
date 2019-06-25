46514e75c0a614c8835d4a360b61b4202b992be1
Karl Palsson <karlp@etactica.com>
2019-06-25 09:27:42 +0000

    doc: efm32: dma: tag headers properly

--- include/libopencm3/efm32/ezr32wg/dma.h
+++ include/libopencm3/efm32/ezr32wg/dma.h
@@ -1,3 +1,11 @@
+/** @defgroup dma_defines DMA Defines
+ *
+ * @brief <b>Defined Constants and Types for the DMA module</b>
+ *
+ * @ingroup EZR32WG_defines
+ *
+ * LGPL License Terms @ref lgpl_license
+ */
 /*
  * This file is part of the libopencm3 project.
  *
@@ -17,9 +25,6 @@
  * along with this library.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#ifndef LIBOPENCM3_EFM32_EZR32WG_DMA_H
-#define LIBOPENCM3_EFM32_EZR32WG_DMA_H
+#pragma once
 
 #include <libopencm3/efm32/common/dma_common.h>
-
-#endif
