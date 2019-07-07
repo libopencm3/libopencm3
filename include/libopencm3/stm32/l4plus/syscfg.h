b259c686b6d0d581790bbbe3e0b601b2454beace
Guillaume Meunier <guillaume.meunier@centraliens.net>
2019-07-07 18:40:04 +0200

    stm32l4plus: add syscfg, mostly shared with stm32l4

--- include/libopencm3/stm32/l4plus/syscfg.h
+++ include/libopencm3/stm32/l4plus/syscfg.h
@@ -1,14 +1,21 @@
 /** @defgroup syscfg_defines SYSCFG Defines
  *
- * @brief <b>Defined Constants and Types for the STM32L4xx Sysconfig</b>
+ * @brief Defined Constants and Types for the STM32L4Rxx/STM32L4Sxx Sysconfig
  *
- * @ingroup STM32L4xx_defines
- * 
+ * @ingroup STM32L4PLUSxx_defines
+ *
+ * @version 1.0.0
+ *
+ * @author @htmlonly &copy; @endhtmlonly 2019 Guillaume Meunier <guillaume.meunier@centraliens.net>
+ *
+ * @date 2 July 2019
+ *
  * LGPL License Terms @ref lgpl_license
  */
-
 /*
  * This file is part of the libopencm3 project.
+ *
+ * Copyright (C) 2019 Guillaume Meunier <guillaume.meunier@centraliens.net>
  *
  * This library is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
@@ -31,6 +38,18 @@
 
 /* --- SYSCFG_MEMRMP Values ------------------------------------------------ */
 
-#define SYSCFG_MEMRMP_MEM_MODE_QSPI     6
+#define SYSCFG_MEMRMP_MEM_MODE_OSPI1	4
+#define SYSCFG_MEMRMP_MEM_MODE_OSPI2	5
+
+/* --- SYSCFG_CFGR1 Values ------------------------------------------------- */
+
+#define SYSCFG_CFGR1_I2C4_FMP		(1 << 23)
+#define SYSCFG_CFGR1_ANASWVDD		(1 << 9)
+
+/* --- SYSCFG_EXTICR Values -------------------------------------------------*/
+
+#define SYSCFG_EXTICR_GPIOF		5
+#define SYSCFG_EXTICR_GPIOG		6
+#define SYSCFG_EXTICR_GPIOI		8
 
 #endif
