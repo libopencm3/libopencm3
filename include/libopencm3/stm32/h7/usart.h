2cc18e94b70b8f7c2c18b8e6dc63e559444b5dcf
Brian Viele <vielster@allocor.tech>
2019-12-17 18:15:04 -0500

    Manual pick of usart updates.

--- include/libopencm3/stm32/h7/usart.h
+++ include/libopencm3/stm32/h7/usart.h
@@ -34,7 +34,6 @@
 #include <libopencm3/stm32/common/usart_common_fifos.h>
 
 /**@{*/
-
 /** @defgroup usart_reg_base USART register base addresses
  * Holds all the U(S)ART peripherals supported.
  * @{
@@ -48,6 +47,12 @@
 #define UART7           UART7_BASE
 #define UART8           UART8_BASE
 /**@}*/
+
+BEGIN_DECLS
+
+END_DECLS
+
 /**@}*/
 
 #endif
+
