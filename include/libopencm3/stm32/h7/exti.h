af765832b45e9a97e1a8b6e739fde08c12fe37d5
Brian Viele <vielster@allocor.tech>
2020-03-20 18:21:38 -0400

    stm32h7: added minimal stm32h7 exti defs, which share with G0.
    Separated definitions that did not seem consistent between the "v2" EXTI
    platforms. Added SYSCFG defs needed for EXTICR settings.

--- include/libopencm3/stm32/h7/exti.h
+++ include/libopencm3/stm32/h7/exti.h
@@ -1,8 +1,8 @@
 /** @defgroup exti_defines EXTI Defines
  *
- * @ingroup STM32G0xx_defines
+ * @ingroup STM32H7xx_defines
  *
- * @brief <b>Defined Constants and Types for the STM32G0xx EXTI Control</b>
+ * @brief <b>Defined Constants and Types for the STM32H7xx EXTI Control</b>
  *
  * @version 1.0.0
  *
@@ -24,7 +24,6 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with this library.  If not, see <http://www.gnu.org/licenses/>.
  */
-
 /**@{*/
 #ifndef LIBOPENCM3_EXTI_H
 #define LIBOPENCM3_EXTI_H
@@ -32,26 +31,13 @@
 #include <libopencm3/stm32/common/exti_common_all.h>
 #include <libopencm3/stm32/common/exti_common_v2.h>
 
-/** EXTI External Interrupt Selection Registers */
-#define EXTI_EXTICR(i)    MMIO32(EXTI_BASE + 0x60 + (i)*4)
-#define EXTI_EXTICR1    MMIO32(EXTI_BASE + 0x60)
-#define EXTI_EXTICR2    MMIO32(EXTI_BASE + 0x64)
-#define EXTI_EXTICR3    MMIO32(EXTI_BASE + 0x68)
-#define EXTI_EXTICR4    MMIO32(EXTI_BASE + 0x6c)
-
-/** EXTI Rising Edge Pending Register */
-#define EXTI_RPR1     MMIO32(EXTI_BASE + 0x0c)
-/** EXTI Falling Edge Pending Register */
-#define EXTI_FPR1     MMIO32(EXTI_BASE + 0x10)
+/** EXTI CPU Event Pending Register 1 */
+#define EXTI_CPUPR1	MMIO32(EXTI_BASE + 0x88)
+#define EXTI_PR 	EXTI_CPUPR1
 
 BEGIN_DECLS
 
 END_DECLS
 
-#else
-/** @cond */
-#warning "exti_common_v1.h should not be included directly, only via exti.h"
-#endif
-/** @endcond */
-
+#endif  /* LIBOPENCM3_EXTI_H */
 /**@}*/
