503cebab87df3b49efbf46a9485f7bafaaeb1b33
Karl Palsson <karlp@tweak.net.au>
2020-04-10 14:12:08 +0000

    stm32g4: turn on FPU in pre_main
    Yep, all M4F need this file added.

--- lib/dispatch/vector_chipset.c
+++ lib/dispatch/vector_chipset.c
@@ -4,6 +4,8 @@
 #	include "../stm32/f4/vector_chipset.c"
 #elif defined(STM32F7)
 #	include "../stm32/f7/vector_chipset.c"
+#elif defined(STM32G4)
+#	include "../stm32/g4/vector_chipset.c"
 #elif defined(STM32L4)
 #	include "../stm32/l4/vector_chipset.c"
 #elif defined(LM4F)
