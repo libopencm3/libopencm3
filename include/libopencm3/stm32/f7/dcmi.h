071d4680ec3f9fc38a617cb267c8bd8d71128f83
Nikolai Smolyaninov <smolin35@gmail.com>
2020-04-22 16:14:04 +0700

    stm32f4/7: DCMI: extract common registers
    And enable for the f7
    
    Originally filed as: https://github.com/libopencm3/libopencm3/pull/1208
    Rviewed-by: Karl Palsson <karlp@tweak.net.au>

--- include/libopencm3/stm32/f7/dcmi.h
+++ include/libopencm3/stm32/f7/dcmi.h
@@ -1,6 +1,21 @@
-/* This provides unification of code over STM32 subfamilies */
+/** @defgroup dcmi_defines DCMI Defines
+ *
+ * @ingroup STM32F7xx_defines
+ *
+ * @brief Defined Constants and Macros for the STM32F7xx DCMI Peripheral
+ *
+ * @version 1.0.0
+ *
+ * @date 2020-05-15
+ *
+ * LGPL License Terms @ref lgpl_license
+ */
+
 
 /*
+ * STM32F7 DCMI Defines
+ * Copyright (C) 2020, Smolyaninov Nikolay <smolin35@gmail.org>
+ *
  * This file is part of the libopencm3 project.
  *
  * This library is free software: you can redistribute it and/or modify
@@ -15,16 +30,28 @@
  *
  * You should have received a copy of the GNU Lesser General Public License
  * along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ *
  */
 
-#include <libopencm3/cm3/common.h>
-#include <libopencm3/stm32/memorymap.h>
+#ifndef LIBOPENCM3_DCMI_H
+#define LIBOPENCM3_DCMI_H
 
-#if defined(STM32F4)
-#       include <libopencm3/stm32/f4/dcmi.h>
-#elif defined(STM32F7)
-#       include <libopencm3/stm32/f7/dcmi.h>
-#else
-#       error "stm32 family not defined."
+#include <libopencm3/stm32/common/dcmi_common_f47.h>
+
+/**@{*/
+
+/**
+ * @defgroup dcmi_cr_values DCMI_CR Values
+ * @ingroup dcmi_defines
+ * @{
+ */
+#define DCMI_CR_OELS			(1 << 20)
+#define DCMI_CR_LSM			(1 << 19)
+#define DCMI_CR_OEBS			(1 << 18)
+#define DCMI_CR_BSM1			(1 << 17)
+#define DCMI_CR_BSM0			(1 << 16)
+
+
+/**@}*/
+
 #endif
-
