b9af86941d7734c6999e6fd6178450fa0778f179
Sam Kirkham <sam.kirkham@codethink.co.uk>
2020-07-29 12:39:49 +0100

    stm32g4: Add support for USB
    Co-authored-by: Ben Brewer <ben.brewer@codethink.co.uk>

--- include/libopencm3/stm32/g4/st_usbfs.h
+++ include/libopencm3/stm32/g4/st_usbfs.h
@@ -1,5 +1,3 @@
-/* This provides unification of USB code for supported STM32 subfamilies */
-
 /*
  * This file is part of the libopencm3 project.
  *
@@ -16,29 +14,14 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with this library.  If not, see <http://www.gnu.org/licenses/>.
  */
+/* THIS FILE SHOULD NOT BE INCLUDED DIRECTLY !
+ * Use top-level <libopencm3/stm32/st_usbfs.h>
+ */
 
 #ifndef LIBOPENCM3_ST_USBFS_H
-#define LIBOPENCM3_ST_USBFS_H
-
-#include <libopencm3/cm3/common.h>
-#include <libopencm3/stm32/memorymap.h>
-
-#if defined(STM32F0)
-#       include <libopencm3/stm32/f0/st_usbfs.h>
-#elif defined(STM32F1)
-#       include <libopencm3/stm32/f1/st_usbfs.h>
-#elif defined(STM32F3)
-#       include <libopencm3/stm32/f3/st_usbfs.h>
-#elif defined(STM32L0)
-#       include <libopencm3/stm32/l0/st_usbfs.h>
-#elif defined(STM32L1)
-#       include <libopencm3/stm32/l1/st_usbfs.h>
-#elif defined(STM32L4)
-#       include <libopencm3/stm32/l4/st_usbfs.h>
-#elif defined(STM32G4)
-#       include <libopencm3/stm32/g4/st_usbfs.h>
+#	error Do not include directly !
 #else
-#       error "STM32 family not defined or not supported."
-#endif
+
+#include <libopencm3/stm32/common/st_usbfs_v2.h>
 
 #endif
