d6201ca806526471c7114ac1c570ba79aa757b18
Nick Downing <nick.downing@lifx.co>
2021-07-22 15:44:41 +1000

    Add USART FIFO capability for STM32L5 (from STM32H7)

--- include/libopencm3/stm32/l5/usart.h
+++ include/libopencm3/stm32/l5/usart.h
@@ -1,8 +1,12 @@
 /** @defgroup usart_defines USART Defines
  *
- * @brief <b>Defined Constants and Types for the STM32L4xx USART</b>
+ * @brief <b>Defined Constants and Types for the STM32H7xx USART</b>
  *
- * @ingroup STM32L4xx_defines
+ * @ingroup STM32H7xx_defines
+ *
+ * @version 1.0.0
+ *
+ * @date 6 November 2019
  *
  * LGPL License Terms @ref lgpl_license
  */
@@ -27,23 +31,25 @@
 #ifndef LIBOPENCM3_USART_H
 #define LIBOPENCM3_USART_H
 
-#include <libopencm3/stm32/common/usart_common_all.h>
-#include <libopencm3/stm32/common/usart_common_v2.h>
+#include <libopencm3/stm32/common/usart_common_fifos.h>
 
+/**@{*/
 /** @defgroup usart_reg_base USART register base addresses
  * Holds all the U(S)ART peripherals supported.
  * @{
  */
-#define USART1				USART1_BASE
-#define USART2				USART2_BASE
-#define USART3				USART3_BASE
-#define UART4				UART4_BASE
-#define UART5				UART5_BASE
-#define LPUART1				LPUART1_BASE
+#define USART1          USART1_BASE
+#define USART2          USART2_BASE
+#define USART3          USART3_BASE
+#define UART4           UART4_BASE
+#define UART5           UART5_BASE
 /**@}*/
 
 BEGIN_DECLS
 
 END_DECLS
 
+/**@}*/
+
 #endif
+
