8581f016716d3ebff3ed63e1b69ff8cea18a1161
dragonmux <git@dragonmux.network>
2022-08-23 03:14:22 +0100

    lm4f/uart: Implemented a function to get the current databits setting for a UART

--- include/libopencm3/lm4f/uart.h
+++ include/libopencm3/lm4f/uart.h
@@ -444,6 +444,7 @@ BEGIN_DECLS
 
 void uart_set_baudrate(uint32_t uart, uint32_t baud);
 void uart_set_databits(uint32_t uart, uint8_t databits);
+uint8_t uart_get_databits(uint32_t uart);
 void uart_set_stopbits(uint32_t uart, uint8_t stopbits);
 void uart_set_parity(uint32_t uart, enum uart_parity parity);
 void uart_set_mode(uint32_t uart, uint32_t mode);
