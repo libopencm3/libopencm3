a4f46833d9df653a778610c21aa046513b1bba27
Sergey Bolshakov <beefdeadbeef@gmail.com>
2022-11-11 23:26:11 +0300

    at32f40x: add rcc bits
    RCC on AT32F40x is similar with STM32F1x series,
    notable differences are (among others):
      * HSE clock in range 4-25 MHz
      * PLL multiplier up to 64x
      * HSI clock 48 MHz, which can be fed to USB
      * MCO output divider, down to /512

--- include/libopencm3/stm32/rcc.h
+++ include/libopencm3/stm32/rcc.h
@@ -44,6 +44,8 @@
 #       include <libopencm3/stm32/g4/rcc.h>
 #elif defined(STM32H7)
 #       include <libopencm3/stm32/h7/rcc.h>
+#elif defined(AT32F40X)
+#       include <libopencm3/at32/f40x/rcc.h>
 #elif defined(GD32F1X0)
 #       include <libopencm3/gd32/f1x0/rcc.h>
 #else
