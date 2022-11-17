libopencm3/libopencm3

commit 18f4d7c1b7c8d4b3eb190827d2b99c4b23162205

stm32f3: rtc: include correct shared header
rtc_common_all never existed, and f3 has the same "v2" peripheral used
by ~all parts other than the f1.  We don't have any f3 rtc test code,
but the existing code was clearly wrong, and this is at least including
the correct basic defines.

Fixes: https://github.com/libopencm3/libopencm3/issues/1106

include/libopencm3/stm32/f3/rtc.h
@@ -34,9 +34,7 @@
 
 #ifndef LIBOPENCM3_RTC_F3_H
 #define LIBOPENCM3_RTC_F3_H
-/**@{*/
 
-#include <libopencm3/stm32/common/rtc_common_all.h>
-/**@}*/
+#include <libopencm3/stm32/common/rtc_common_l1f024.h>
 
 #endif

commit 7c09d0d14c99d5be56436a3864038212812bcd2a

stm32f3: enable RTC
Fixes: https://github.com/libopencm3/libopencm3/issues/1446
Fixes: 18f4d7c1b7 stm32f3: rtc: include correct shared header

No testing on real hardware, but the docs imply that this should have
always had the "basic" rtc-v2 peripheral.

Signed-off-by: Karl Palsson <karlp@tweak.net.au>

include/libopencm3/stm32/f3/rtc.h
@@ -32,8 +32,8 @@
  * along with this library.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#ifndef LIBOPENCM3_RTC_F3_H
-#define LIBOPENCM3_RTC_F3_H
+#ifndef LIBOPENCM3_RTC_H
+#define LIBOPENCM3_RTC_H
 
 #include <libopencm3/stm32/common/rtc_common_l1f024.h>
 

lib/stm32/f3/Makefile
@@ -49,6 +49,7 @@ OBJS += iwdg_common_all.o
 OBJS += opamp_common_all.o opamp_common_v1.o
 OBJS += pwr_common_v1.o
 OBJS += rcc.o rcc_common_all.o
+OBJS += rtc_common_l1f024.o
 OBJS += spi_common_all.o spi_common_v2.o
 OBJS += timer_common_all.o timer_common_f0234.o
 OBJS += usart_common_v2.o usart_common_all.o