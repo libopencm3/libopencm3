34906fe9f6a55f398062c8015792474fa1608eb9
Sergey Bolshakov <beefdeadbeef@gmail.com>
2022-11-26 21:00:02 +0300

    at32f40x: add dma support
    In addition to STM32F1 DMA controllers fuctionality,
    AT32F4x has so-called flexible request mapping mode,
    resembling channel remapping feature found on F0 MCUs,
    although implemented differently.
    Add support for it.

--- include/libopencm3/stm32/dma.h
+++ include/libopencm3/stm32/dma.h
@@ -42,6 +42,8 @@
 #       include <libopencm3/stm32/g0/dma.h>
 #elif defined(STM32G4)
 #       include <libopencm3/stm32/g4/dma.h>
+#elif defined(AT32F40X)
+#	include <libopencm3/at32/f40x/dma.h>
 #else
 #       error "stm32 family not defined."
 #endif
