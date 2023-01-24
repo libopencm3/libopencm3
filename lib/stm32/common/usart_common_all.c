7e6f4e8f90b1f9664918e7c1e6a78c9b649692c2
Harry Geyer <harry.geyer@devtank.co.uk>
2023-01-24 12:13:54 +0000

    Fix. Remove the check if usart is USART1 or USART6 after getting the clockspeed.

--- lib/stm32/common/usart_common_all.c
+++ lib/stm32/common/usart_common_all.c
@@ -54,16 +54,6 @@ void usart_set_baudrate(uint32_t usart, uint32_t baud)
 {
 	uint32_t clock = rcc_get_usart_clk_freq(usart);
 
-#if defined USART1
-	if ((usart == USART1)
-#if defined USART6
-		|| (usart == USART6)
-#endif
-		) {
-		clock = rcc_apb2_frequency;
-	}
-#endif
-
 	/*
 	 * Yes it is as simple as that. The reference manual is
 	 * talking about fractional calculation but it seems to be only
