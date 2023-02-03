c5cdeee67ec17b0c2a45684877d106b1a954db6f
Sebastien Lorquet <sebastien@lorquet.fr>
2023-02-03 15:32:46 +0100

    Enable required files to ensure successful build of a "stm32h7" only lib.

--- lib/stm32/common/usart_common_all.c
+++ lib/stm32/common/usart_common_all.c
@@ -52,17 +52,7 @@ usart_reg_base
 
 void usart_set_baudrate(uint32_t usart, uint32_t baud)
 {
-	uint32_t clock = rcc_apb1_frequency;
-
-#if defined USART1
-	if ((usart == USART1)
-#if defined USART6
-		|| (usart == USART6)
-#endif
-		) {
-		clock = rcc_apb2_frequency;
-	}
-#endif
+	uint32_t clock = rcc_get_usart_clk_freq(usart);
 
 	/*
 	 * Yes it is as simple as that. The reference manual is
