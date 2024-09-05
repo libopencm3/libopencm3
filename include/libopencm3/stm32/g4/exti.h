7bc6d204e81bf093c89b7271e3f157492103ed34
Stéphane Clérambault <stephane.clerambault@radiko.fr>
2024-09-05 13:00:28 +0200

    stm32g4: clean up exti header and add exti_common_all.c to Makefile.

--- include/libopencm3/stm32/g4/exti.h
+++ include/libopencm3/stm32/g4/exti.h
@@ -25,33 +25,10 @@
  * along with this library.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-/**@{*/
 #ifndef LIBOPENCM3_EXTI_H
 #define LIBOPENCM3_EXTI_H
 
 #include <libopencm3/stm32/common/exti_common_all.h>
-#include <libopencm3/stm32/common/exti_common_v2.h>
+#include <libopencm3/stm32/common/exti_common_v1.h>
 
-/** EXTI External Interrupt Selection Registers */
-#define EXTI_EXTICR(i)    MMIO32(EXTI_BASE + 0x60 + (i)*4)
-#define EXTI_EXTICR1    MMIO32(EXTI_BASE + 0x60)
-#define EXTI_EXTICR2    MMIO32(EXTI_BASE + 0x64)
-#define EXTI_EXTICR3    MMIO32(EXTI_BASE + 0x68)
-#define EXTI_EXTICR4    MMIO32(EXTI_BASE + 0x6c)
-
-/** EXTI Rising Edge Pending Register */
-#define EXTI_RPR1     MMIO32(EXTI_BASE + 0x0c)
-/** EXTI Falling Edge Pending Register */
-#define EXTI_FPR1     MMIO32(EXTI_BASE + 0x10)
-
-BEGIN_DECLS
-
-END_DECLS
-
-#else
-/** @cond */
-#warning "exti_common_v1.h should not be included directly, only via exti.h"
 #endif
-/** @endcond */
-
-/**@}*/
