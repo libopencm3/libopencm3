7a87069bf4741524a816ff3add15a9d5b9f9b22e
Mateusz Myalski <mateusz.myalski@gmail.com>
2024-10-07 18:22:59 +0200

    Added minimal memory map gpi and rcc to blink LED

--- include/libopencm3/stm32/gpio.h
+++ include/libopencm3/stm32/gpio.h
@@ -44,6 +44,8 @@
 #       include <libopencm3/stm32/g4/gpio.h>
 #elif defined(STM32H7)
 #       include <libopencm3/stm32/h7/gpio.h>
+#elif defined(STM32U5)
+#       include <libopencm3/stm32/u5/gpio.h>
 #elif defined(GD32F1X0)
 #       include <libopencm3/gd32/f1x0/gpio.h>
 #else
