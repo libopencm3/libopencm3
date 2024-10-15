3a1275bb076af50a4bccfe1e1b4bcdc69e2a5790
Roman Buchert <roman.buchert@googlemail.com>
2024-10-15 15:18:52 +0200

    Add functions for basic register access.

--- include/libopencm3/stm32/common/quadspi_common_v1.h
+++ include/libopencm3/stm32/common/quadspi_common_v1.h
@@ -1,5 +1,6 @@
 /** @addtogroup quadspi_defines
  * @author Chuck McManis <cmcmanis@mcmanis.com> 2016
+ * @author Roman Buchert <libopencm3@ro-bu.net> 2024
  * @copyright SPDX: LGPL-3.0-or-later
  * @{
  */
@@ -56,11 +57,21 @@
 #define QUADSPI_CR_PMM      (1 << 23)
 #define QUADSPI_CR_APMS     (1 << 22)
 /* bit 21 is reserved */
+
+/**
+@defgroup quadspi_irq_enable QUADSPI_CR interrupt enable values
+@{*/
+/* TOIE:*/ /** Timeout interrupt enable */
 #define QUADSPI_CR_TOIE     (1 << 20)
+/* SMIE:*/ /** Status match interrupt enable */
 #define QUADSPI_CR_SMIE     (1 << 19)
+/* FTIE:*/ /** FIFO threshold interrupt enable */
 #define QUADSPI_CR_FTIE     (1 << 18)
+/* TCIE:*/ /** Transfer complete interrupt enable */
 #define QUADSPI_CR_TCIE     (1 << 17)
+/* TEIE*/ /** Transfer error interrupt enable */
 #define QUADSPI_CR_TEIE     (1 << 16)
+/**@}*/
 
 /* bits 15:13 reserved */
 #define QUADSPI_CR_FTHRES_MASK    0x1f
@@ -88,20 +99,37 @@
 #define QUADSPI_SR_FLEVEL_SHIFT   8
 
 /* bits 7:6 reserved */
+/**
+@defgroup quadspi_sr_values QUADSPI status register values
+@{*/
+/* BUSY:*/ /** Busy flag*/
 #define QUADSPI_SR_BUSY     (1 << 5)
+/* TOF:*/ /** Timeout flag*/
 #define QUADSPI_SR_TOF      (1 << 4)
+/* SMF:*/ /** Status match flag */
 #define QUADSPI_SR_SMF      (1 << 3)
+/* FTF:*/ /** FIFO threshold flag */
 #define QUADSPI_SR_FTF      (1 << 2)
+/* TCF:*/ /** Transfer complete flag */
 #define QUADSPI_SR_TCF      (1 << 1)
+/* TEF:*/ /** Transfer error flag */
 #define QUADSPI_SR_TEF      (1 << 0)
+/**@}*/
 
+/**
+@defgroup quadspi_fcr_values QUADSPI flag clear register values
+@{*/
 /* bits 31:5 reserved */
+/* CTOF:*/ /** Clear timeout flag */
 #define QUADSPI_FCR_CTOF    (1 << 4)
+/* CSMF:*/ /** Clear status match flag */
 #define QUADSPI_FCR_CSMF    (1 << 3)
 /* bit 2 reserved */
+/* CTCF:*/ /** Clear transfer complete flag */
 #define QUADSPI_FCR_CTCF    (1 << 1)
+/* CTEF:*/ /** Clear transfer error flag */
 #define QUADSPI_FCR_CTEF    (1 << 0)
-
+/**@}*/
 #define QUADSPI_CCR_DDRM    (1 << 31)
 #define QUADSPI_CCR_DHHC    (1 << 30)
 /* bit 29 reserved on F4, FRCM on H7 */
@@ -133,17 +161,85 @@
 #define QUADSPI_CCR_INST_SHIFT    0
 
 /* MODE values */
+/**
+@defgroup quadspi_ccr_mode QUADSPI_CCR_MODE values
+@{*/
+/** No data */
 #define QUADSPI_CCR_MODE_NONE   0
+/** Use one line for communication */
 #define QUADSPI_CCR_MODE_1LINE    1
+/** Use two lines for communication */
 #define QUADSPI_CCR_MODE_2LINE    2
+/** Use four lines for communication */
 #define QUADSPI_CCR_MODE_4LINE    3
+/**@}*/
+
+/* SIZE values */
+/**
+@defgroup quadspi_ccr_size QUADSPI_CCR_SIZE values
+@{*/
+/** Size is 8Bit (1 Byte) */
+#define QUADSPI_CCR_SIZE_8BIT   0
+/** Size is 16Bit (2 Byte) */
+#define QUADSPI_CCR_SIZE_16BIT  1
+/** Size is 24Bit (3 Byte) */
+#define QUADSPI_CCR_SIZE_24BIT  2
+/** Size is 32Bit (4 Byte) */
+#define QUADSPI_CCR_SIZE_32BIT  3
+/**@}*/
 
 /* FMODE values */
+/**
+@defgroup quadspi_ccr_fmode QUADSPI_CCR_FMODE values
+@{*/
+/** Indirection write mode */
 #define QUADSPI_CCR_FMODE_IWRITE  0
+/** Indirection read mode */
 #define QUADSPI_CCR_FMODE_IREAD   1
+/** Automatic polling mode */
 #define QUADSPI_CCR_FMODE_APOLL   2
+/** Memory-mapped mode */
 #define QUADSPI_CCR_FMODE_MEMMAP  3
+/**@}*/
 
+/* Select flash memory */
+#define QUADSPI_FLASH_SEL_1       2
+#define QUADSPI_FLASH_SEL_2       3
+#define QUADSPI_FLASH_SEL_DUAL    1
+
+/**
+@defgroup quadspi_ckmode QUADSPI_DCR_CKMODE values
+@{*/
+#define QUADSPI_DCR_CKMOD_MODE0    0
+#define QUADSPI_DCR_CDMOD_MODE3    QUADSPI_DCR_CKMODE
+/**@}*/
+
+/**
+@defgroup quadspi_ccr_ddrm QUADSPI_CCR_DDRM values
+@{*/
+/** Disable the double-data-rate mode */
+#define QUADSPI_CCR_DDRM_DISABLE    0
+/** Enable the double-data-rate mode */
+#define QUADSPI_CCR_DDRM_ENABLE     1
+/**@}*/
+
+/**
+@defgroup quadspi_ccr_dhhc QUADSPI_CCR_DHHC values
+@{*/
+/** Delay the data output using analog delay */
+#define QUADSPI_CCR_DHHC_ANALOG     0
+/** Delay the data output by 1/4 of a QUADSPI output clock cycle */
+#define QUADSPI_CCR_DHHC_1_4_CLK    1
+/**@}*/
+
+/**
+@defgroup quadspi_ccr_sioo QUADSPI_CCR_SIOO values
+@{*/
+/** Send instruction on every transaction */
+#define QUADSPI_CCR_SIOO_EVERY      0
+/** Send instruction only for the first command */
+#define QUADSPI_CCR_SIOO_ONCE       1
+/**@}*/
 /**@}*/
 
 
@@ -166,9 +262,319 @@ BEGIN_DECLS
 void quadspi_enable(void);
 
 /**
- * Disable the quadspi peripheral.
- */
+Disable the quadspi peripheral.
+*/
 void quadspi_disable(void);
+
+/**
+@brief Test if the QUADSPI interface is busy
+
+@return boolean: On set: Interface is busy.
+*/
+bool quadspi_is_busy(void);
+
+/**
+@brief Wait as long as the QUADSPI interface is busy
+
+*/
+void quadspi_wait_while_busy(void);
+
+/**
+Abort the communication.
+*/
+void quadspi_abort(void);
+
+/**
+@brief Set the prescaler for the QUADSPI clock.
+
+The prescaler is derived from the AHB clock.
+This function must only be called while the interface isn't busy.
+
+As described in RM0385 the real prescaler ist the value+1.
+
+@param[in] prescaler Unsigned int8. Clock prescaler (value+1)
+*/
+void quadspi_set_prescaler(uint8_t prescaler);
+
+/**
+@brief Enable interrupts for QUADSPI
+
+@param[in] irq Unsigned int32. @ref quadspi_irq_enable. Logical OR of all interrupt
+enable bits to be set
+*/
+void quadspi_enable_irq(uint32_t irq);
+
+/**
+@brief Disable interrupts for QUADSPI
+
+@param[in] irq Unsigned int32. @ref quadspi_irq_enable. Logical OR of all interrupt
+enable bits to be cleared
+*/
+void quadspi_disable_irq(uint32_t irq);
+
+/**
+@brief Set the clockmode for QUADSPI
+Valid modes are mode 0 and mode 3.
+
+Mode 0: CLK must stay low while nCS is high.
+Mode 3: CLK must stay hight while nCS is hight.
+
+@param mode Unsigned int32. @ref quadspi_ckmode
+*/
+void quadspi_set_clock_mode(uint32_t mode);
+
+/**
+@brief Set the Chip select hight time.
+
+Defines the minimum number of CLK cycles which the nCS must remain high between commands
+ussued to the FLASH memory.
+
+As in RM0385 described, the number of CLK cycles is cycles+1.
+
+@param[in] cycles Unsigned int32. Number+1 cycles of nCS high between two FLASH memory commands.
+*/
+void quadspi_set_cs_high_time(uint32_t cycles);
+
+/**
+@brief Flash memory size
+
+This defines the size of the external memory using the following formula:
+
+Number of bytes in Flash memory = 2^(flash_size+1)
+
+Flash_size+1 is effectivly the number of address bits required to address the FLASH memory.
+
+If dual-flash-mode is enabled, the size indicates the total capacity
+of both FLASH memories together.
+
+@param[in] flash_size Unsigned int32. The FLASH memory size in 2^(flash_size+1)
+ */
+void quadspi_set_flash_size(uint32_t flash_size);
+
+
+/**
+@brief Read status flag.
+
+@param[in] flag Unsigned int32. Status register flag. @ref quadspi_sr_values
+@return boolean: flag set
+*/
+bool quadspi_get_flag(uint32_t flag);
+
+/**
+@brief Get the number of valid bytes in FIFO
+
+Return the number of valid bytes which are beeing helod in the FIFO.
+
+FLEVEL=[0..32]: FIFO level from empty to full.
+
+@return uint8_t: Number of valid bytes held in FIFO.
+*/
+uint8_t quadspi_get_fifo_level(void);
+
+/**
+@brief Clear a status flag
+
+@param[in] flag Unsigned int32. Flag to be cleared. @ref qspi_fcr_values
+*/
+void quadspi_clear_flag(uint32_t flag);
+
+
+/**
+@brief Set the number of bytes to be transfered.
+
+Number of data to be retrieved (value+1) in indirection and status-polling mode.
+A value no greater than 3 (inidcating 4 bytes) should be used for status-polling mode.
+
+A value of 0xFFFFFFFF in indirect mode means undefined length, where QUADSPI will continue
+until the end of memory, as defined by FSIZE.
+
+@param[in] Unsigned int32. data_length Nuber of bytes+1 to be transfered.
+*/
+void quadspi_set_data_length(uint32_t data_length);
+
+/**
+@brief Set the double-data-rate mode
+
+Set the DDR mode for the address, alternate byte and data phase.
+
+@param[in] set Boolean. @ref quadspi_ccr_ddrm
+*/
+void quadspi_set_double_datarate_mode(bool set);
+
+/**
+@brief Set the double-data-rate-hold mode
+
+Delay the data output by 1/4 of the QUADSPI output clock cycle in DDR mode.
+
+This bit is only active in DDR mode enabled.
+
+@param[in] set Boolean. @ref quadspi_ccr_dhhc
+ */
+void quadspi_set_ddr_hold(bool set);
+
+/**
+@brief Set the send instruction only once mode
+
+Some Flash memories (e.g. Winbound) might provide a mode where an instruction must be
+sent only with the first command sequence, while subsequent commands start directly with
+the address. One can take advantage of such a feature using the SIOO bit
+
+SIOO is valid for all functional modes (indirect, automatic polling, and memory-mapped). If
+the SIOO bit is set, the instruction is sent only for the first command following a write to
+QUADSPI_CCR. Subsequent command sequences skip the instruction phase, until there is
+a write to QUADSPI_CCR.
+SIOO has no effect when IMODE = 00 (no instruction).
+
+@param[in] set Boolean. @ref quadspi_ccr_sioo
+*/
+void quadspi_set_instruction_only_once_mode(bool set);
+
+/**
+@brief Set the functional mode
+
+Defines the QUADSPI functional mode of operation.
+
+@param[in] mode Unsigned int32. Set the function mode to indirect read/write, automatic polling
+or memory-mapped mode. @ref quadspi_rcc_fmode
+*/
+void quadspi_set_funcion_mode(uint32_t mode);
+
+
+/**
+@brief Set the data phase's mode
+
+Set the QUADSPI data phase's mode of operation.
+
+@param[in] mode Unsigned int32. Set the data phases's mode to No data, 1-line, 2-lines, 4-lines.
+@ref quadspi_rcc_fmode
+*/
+void quadspi_set_data_mode(uint32_t mode);
+
+/**
+@brief Set the number of dummy cycles
+
+Defines the duration of the dummy phase. In both SDR and DDR modes, it specifies
+the number of CLK cycles (0..31).
+
+@param[in] cycles Unsinged int32. Number of dummy CLK cycles (0..31).
+*/
+void quadspi_set_dummy_cycles(uint32_t cycles);
+
+/**
+@brief Set the alternate-byte size
+
+This defines the alternate byte size
+
+@param[in] size Unsigned int32. Number of alternate bytes to be sent. @ref quadspi_ccr_size
+ */
+void quadspi_set_alternate_byte_size(uint32_t size);
+
+
+/**
+@brief set the alternate-byte mode
+
+This defines the alternate-byte phase mode of operation.
+
+@param[in] mode Unsigned int32. Set the alternate byte phases mode to
+No data, 1-line, 2-lines, 4-lines.
+@ref quadspi_rcc_fmode
+*/
+void quadspi_set_alternate_byte_mode(uint32_t mode);
+
+/**
+@brief Set the address size
+
+This defines the address size
+
+@param[in] size Unsigned int32. Number of address bytes to be sent. @ref quadspi_ccr_size
+ */
+void quadspi_set_address_size(uint32_t size);
+
+/**
+@brief set the address mode
+
+This defines the address phase mode of operation.
+
+@param[in] mode Unsigned int32. Set the address phases mode to No data, 1-line, 2-lines, 4-lines.
+@ref quadspi_rcc_fmode
+*/
+void quadspi_set_address_mode(uint32_t mode);
+
+/**
+@brief set the instruction mode
+
+This defines the instruction phase mode of operation.
+
+@param[in] mode Unsigned int32. Set the instruction phases mode to
+No data, 1-line, 2-lines, 4-lines.
+@ref quadspi_rcc_fmode
+*/
+void quadspi_set_instruction_mode(uint32_t mode);
+
+/**
+@brief Send instruction
+
+Instruction to be send to the external SPI device.
+
+@param[in] instruction Unsigned int8. Instruction to be send.
+*/
+void quadspi_send_instruction(uint8_t instruction);
+
+/**
+@brief Set the address to be send to the external FLASH memory
+
+@param[in] address Unsigned int32. Address to be send to the FLASH memory
+*/
+void quadspi_set_address(uint32_t address);
+
+/**
+@brief Set the alternate-byte to be send.
+
+@param[in] data Unsigned int32. Alternate-byte value
+*/
+void quadspi_set_alternate_byte(uint32_t data);
+
+/**
+@brief Write one data-byte to the data register
+
+@param[in] data Unsigned int8. One byte to be written
+*/
+void quadspi_write_byte(uint8_t data);
+
+/**
+@brief Write two data-bytes to the data register
+
+@param[in] data Unsigned int16. Two bytes to be written
+*/
+void quadspi_write_halfword(uint16_t data);
+
+/**
+@brief Write four data-bytes to the data register
+
+@param[in] data Unsigned int16. Four bytes to be written
+*/
+void quadspi_write_word(uint32_t data);
+
+/**
+@brief Read one byte from the data register
+
+@return uint8_t One byte of data
+*/
+uint8_t quadspi_read_byte(void);
+
+/**
+@brief Read two bytes from the data register
+
+@return uint16_t Two bytes of data
+*/
+uint16_t quadspi_read_halfword(void);
+
+/**
+@brief Read four byte from the data register
+
+@return uint32_t four bytes of data
+*/
+uint32_t quadspi_read_word(void);
 
 END_DECLS
 
--- lib/stm32/common/quadspi_common_v1.c
+++ lib/stm32/common/quadspi_common_v1.c
@@ -1,3 +1,20 @@
+/** @addtogroup quadspi_file QUADSPI peripheral API
+@ingroup peripheral_apis
+
+@author @htmlonly &copy; @endhtmlonly 2016
+Chuck McManis <cmcmanis@mcmanis.com>
+@author @htmlonly &copy; @endhtmlonly 2024
+Roman Buchert <libopencm3@ro-bu.net>
+
+@section  quadspi_common Notes for QUADSPI
+
+This library supports basic access to the QUADSPI registers.
+
+Pay attention, that some function must only be called, when the busy flag is not set.
+
+Further improvements will be taken to provide high level functions for configuration
+and data transfer.
+*/
 
 #include <libopencm3/stm32/quadspi.h>
 
@@ -10,3 +27,205 @@ void quadspi_disable(void)
 {
 	QUADSPI_CR &= ~QUADSPI_CR_EN;
 }
+
+bool quadspi_is_busy(void)
+{
+	return (QUADSPI_SR & QUADSPI_SR_BUSY) ? 1 : 0;
+}
+
+void quadspi_wait_while_busy(void)
+{
+	while (quadspi_is_busy()) {
+		;
+	}
+}
+
+void quadspi_abort(void)
+{
+	QUADSPI_CR |= QUADSPI_CR_ABORT;
+
+	/* Wait for abort is finished. Descriped in Section 14.3.14 on RM0385 Rev 8*/
+	while ((QUADSPI_CR & QUADSPI_CR_ABORT)) {
+		;
+	}
+	quadspi_wait_while_busy();
+}
+
+void quadspi_set_prescaler(uint8_t prescaler)
+{
+	uint32_t reg_value = QUADSPI_CR;
+	reg_value &= ~(QUADSPI_CR_PRESCALE_MASK << QUADSPI_CR_PRESCALE_SHIFT);
+	reg_value |= ((uint32_t)prescaler << QUADSPI_CR_PRESCALE_SHIFT);
+	QUADSPI_CR = reg_value;
+}
+
+void quadspi_enable_irq(uint32_t irq)
+{
+	QUADSPI_CR |= irq;
+}
+
+void quadspi_disable_irq(uint32_t irq)
+{
+	QUADSPI_CR &= ~irq;
+}
+
+bool quadspi_get_flag(uint32_t flag)
+{
+	return (bool)((QUADSPI_SR & flag) ? 1 : 0);
+}
+
+uint8_t quadspi_get_fifo_level(void)
+{
+	return (uint8_t)((QUADSPI_SR >> QUADSPI_SR_FLEVEL_SHIFT) & QUADSPI_SR_FLEVEL_MASK);
+}
+
+void quadspi_clear_flag(uint32_t flag)
+{
+	QUADSPI_FCR |= flag;
+}
+
+void quadspi_set_data_length(uint32_t data_length)
+{
+	QUADSPI_FCR = data_length;
+}
+
+void quadspi_set_double_datarate_mode(bool set)
+{
+
+	if (set == QUADSPI_CCR_DDRM_ENABLE)	{
+		QUADSPI_CCR |= QUADSPI_CCR_DDRM;
+	} else {
+		QUADSPI_CCR &= ~QUADSPI_CCR_DDRM;
+	}
+}
+
+void quadspi_set_ddr_hold(bool set)
+{
+	if (set == QUADSPI_CCR_DHHC_1_4_CLK) {
+		QUADSPI_CCR |= QUADSPI_CCR_DHHC;
+	} else {
+		QUADSPI_CCR &= ~QUADSPI_CCR_DHHC;
+	}
+}
+
+void quadspi_set_instruction_only_once_mode(bool set)
+{
+	if (set == QUADSPI_CCR_SIOO_ONCE) {
+		QUADSPI_CCR |= QUADSPI_CCR_SIOO;
+	} else {
+		QUADSPI_CCR &= ~QUADSPI_CCR_SIOO;
+	}
+}
+
+void quadspi_set_funcion_mode(uint32_t mode)
+{
+	uint32_t reg_val = QUADSPI_CCR;
+	reg_val &= ~(QUADSPI_CCR_FMODE_MASK << QUADSPI_CCR_FMODE_SHIFT);
+	reg_val |= (mode & QUADSPI_CCR_FMODE_MASK) << QUADSPI_CCR_FMODE_SHIFT;
+	QUADSPI_CCR = reg_val;
+}
+
+void quadspi_set_data_mode(uint32_t mode)
+{
+	uint32_t reg_val = QUADSPI_CCR;
+	reg_val &= ~(QUADSPI_CCR_DMODE_MASK << QUADSPI_CCR_DMODE_SHIFT);
+	reg_val |= (mode & QUADSPI_CCR_DMODE_MASK) << QUADSPI_CCR_DMODE_SHIFT;
+	QUADSPI_CCR = reg_val;
+}
+
+void quadspi_set_dummy_cycles(uint32_t cycles)
+{
+	uint32_t reg_val = QUADSPI_CCR;
+	reg_val &= ~(QUADSPI_CCR_DCYC_MASK << QUADSPI_CCR_DCYC_SHIFT);
+	reg_val |= (cycles & QUADSPI_CCR_DCYC_MASK) << QUADSPI_CCR_DCYC_SHIFT;
+	QUADSPI_CCR = reg_val;
+}
+
+void quadspi_set_alternate_byte_size(uint32_t size)
+{
+	uint32_t reg_val = QUADSPI_CCR;
+	reg_val &= ~(QUADSPI_CCR_ABSIZE_MASK << QUADSPI_CCR_ABSIZE_SHIFT);
+	reg_val |= (size & QUADSPI_CCR_ABSIZE_MASK) << QUADSPI_CCR_ABSIZE_SHIFT;
+	QUADSPI_CCR = reg_val;
+}
+
+void quadspi_set_alternate_byte_mode(uint32_t mode)
+{
+	uint32_t reg_val = QUADSPI_CCR;
+	reg_val &= ~(QUADSPI_CCR_ABMODE_MASK << QUADSPI_CCR_ABMODE_SHIFT);
+	reg_val |= (mode & QUADSPI_CCR_ABMODE_MASK) << QUADSPI_CCR_ABMODE_SHIFT;
+	QUADSPI_CCR = reg_val;
+}
+
+void quadspi_set_address_size(uint32_t size)
+{
+	uint32_t reg_val = QUADSPI_CCR;
+	reg_val &= ~(QUADSPI_CCR_ADSIZE_MASK << QUADSPI_CCR_ADSIZE_SHIFT);
+	reg_val |= (size & QUADSPI_CCR_ADSIZE_MASK) << QUADSPI_CCR_ADSIZE_SHIFT;
+	QUADSPI_CCR = reg_val;
+}
+
+void quadspi_set_address_mode(uint32_t mode)
+{
+	uint32_t reg_val = QUADSPI_CCR;
+	reg_val &= ~(QUADSPI_CCR_ADMODE_MASK << QUADSPI_CCR_ADMODE_SHIFT);
+	reg_val |= (mode & QUADSPI_CCR_ADMODE_MASK) << QUADSPI_CCR_ADMODE_SHIFT;
+	QUADSPI_CCR = reg_val;
+}
+
+void quadspi_set_instruction_mode(uint32_t mode)
+{
+	uint32_t reg_val = QUADSPI_CCR;
+	reg_val &= ~(QUADSPI_CCR_IMODE_MASK << QUADSPI_CCR_IMODE_SHIFT);
+	reg_val |= (mode & QUADSPI_CCR_IMODE_MASK) << QUADSPI_CCR_IMODE_SHIFT;
+	QUADSPI_CCR = reg_val;
+}
+
+void quadspi_send_instruction(uint8_t instruction)
+{
+	uint32_t reg_val = QUADSPI_CCR;
+	reg_val &= ~(QUADSPI_CCR_INST_MASK << QUADSPI_CCR_INST_SHIFT);
+	reg_val |= (instruction & QUADSPI_CCR_INST_MASK) << QUADSPI_CCR_INST_SHIFT;
+	QUADSPI_CCR = reg_val;
+}
+
+void quadspi_set_address(uint32_t address)
+{
+	QUADSPI_AR = address;
+}
+
+void quadspi_set_alternate_byte(uint32_t data)
+{
+	QUADSPI_ABR = data;
+}
+
+void quadspi_write_byte(uint8_t data)
+{
+	QUADSPI_DR = data;
+}
+
+void quadspi_write_halfword(uint16_t data)
+{
+	QUADSPI_DR = data;
+}
+
+void quadspi_write_word(uint32_t data)
+{
+	QUADSPI_DR = data;
+}
+
+uint8_t quadspi_read_byte(void)
+{
+	return (uint8_t) QUADSPI_DR;
+}
+
+uint16_t quadspi_read_halfword(void)
+{
+	return (uint16_t) QUADSPI_DR;
+}
+
+uint32_t quadspi_read_word(void)
+{
+	return (uint32_t) QUADSPI_DR;
+}
+
