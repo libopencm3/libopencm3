44d3bae7242e300fcf670d1e999cc7c453817b66
Sergey Bolshakov <beefdeadbeef@gmail.com>
2024-11-23 23:36:05 +0300

    at32f43x: add dmamux bits

--- include/libopencm3/stm32/dmamux.h
+++ include/libopencm3/stm32/dmamux.h
@@ -24,6 +24,8 @@
 #       include <libopencm3/stm32/g0/dmamux.h>
 #elif defined(STM32G4)
 #       include <libopencm3/stm32/g4/dmamux.h>
+#elif defined(AT32F43X)
+#       include <libopencm3/at32/f43x/dmamux.h>
 #else
 #       error "stm32 family not defined."
 #endif
