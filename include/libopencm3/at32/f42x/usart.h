200114b51c51a059f9ec2484f3c2ac6a97565551
Sergey Bolshakov <beefdeadbeef@gmail.com>
2025-03-05 18:02:07 +0300

    at32f42x: add missing usart definitions

--- include/libopencm3/at32/f42x/usart.h
+++ include/libopencm3/at32/f42x/usart.h
@@ -33,8 +33,40 @@ LGPL License Terms @ref lgpl_license
 
 #include <libopencm3/stm32/common/usart_common_f124.h>
 
+/** @defgroup usart_reg_base USART register base addresses
+ * Holds all the U(S)ART peripherals supported.
+ * @{
+ */
+#define USART4			USART4_BASE
+/**@}*/
+
+/* --- USART_CR1 (USART_CTRL1) values -------------------------------------- */
+#define USART_CR1_M1		(1 << 28)		/* DBN1 */
+
+#define USART_CR1_DEAT_SHIFT	21
+#define USART_CR1_DEAT		(0x1F << USART_CR1_DEAT_SHIFT)
+/** DEAT[4:0]: Driver Enable assertion time */
+#define USART_CR1_DEAT_VAL(x)	((x) << USART_CR1_DEAT_SHIFT)
+
+#define USART_CR1_DEDT_SHIFT	16
+#define USART_CR1_DEDT		(0x1F << USART_CR1_DEDT_SHIFT)
+/** DEDT[4:0]: Driver Enable deassertion time */
+#define USART_CR1_DEDT_VAL(x)	((x) << USART_CR1_DEDT_SHIFT)
+
+#define USART_CR1_M0		(1 << 12)		/* DBN0 */
+
+/* --- USART_CR2 (USART_CTRL2) values -------------------------------------- */
+/** ADD[7:4]: Address of the USART node. */
+#define USART_CR2_ADD_SHIFT	28
+
+/** SWAP: Swap TX/RX pins */
+#define USART_CR2_SWAP		(1 << 15)		/* TRPSWAP */
+
+/** ADDM7:7-bit Address Detection/4-bit Address Detection */
+#define USART_CR2_ADDM7		(1 << 4)		/* IDBN */
+
 /* --- USART_CR3 (USART_CTRL3) values -------------------------------------- */
 #define USART_CR3_DEP		(1 << 15)		/* DEP */
-#define USART_CR3_RS485E	(1 << 14)		/* RS485EN */
+#define USART_CR3_DEM		(1 << 14)		/* RS485EN */
 
 #endif
