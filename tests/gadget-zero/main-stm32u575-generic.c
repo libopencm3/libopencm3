7be494de2bef418300ae76259844068e9c70ddbb
ALTracer <11005378+ALTracer@users.noreply.github.com>
2025-09-21 22:08:32 +0300

    tests: gadget0: include newly added CRS

--- tests/gadget-zero/main-stm32u575-generic.c
+++ tests/gadget-zero/main-stm32u575-generic.c
@@ -20,6 +20,7 @@
 #include <libopencm3/cm3/nvic.h>
 #include <libopencm3/stm32/gpio.h>
 #include <libopencm3/stm32/rcc.h>
+#include <libopencm3/stm32/crs.h>
 #include <libopencm3/usb/dwc/otg_fs.h>
 
 #include <stdio.h>
