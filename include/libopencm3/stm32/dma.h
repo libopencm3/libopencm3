7fbddb9671386e9cf447797a93ff58d2530530e6
dragonmux <git@dragonmux.network>
2025-11-03 17:13:43 +0000

    stm32/u5: Implemented support for the GPDMA and LPDMA peripherals

--- include/libopencm3/stm32/dma.h
+++ include/libopencm3/stm32/dma.h
@@ -44,6 +44,8 @@
 #       include <libopencm3/stm32/g4/dma.h>
 #elif defined(STM32H7)
 #       include <libopencm3/stm32/h7/dma.h>
+#elif defined(STM32U5)
+#       include <libopencm3/stm32/u5/dma.h>
 #else
 #       error "stm32 family not defined."
 #endif
