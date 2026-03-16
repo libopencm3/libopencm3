c5022b86197ac58247e0a473dbf3e32f184e6eb1
dragonmux <git@dragonmux.network>
2026-03-16 17:18:46 +0000

    tests/gadget-zero: Provided SWO setup so the debug information the program creates has somewhere to go and be extracted from

--- tests/gadget-zero/main-stm32f411-generic.c
+++ tests/gadget-zero/main-stm32f411-generic.c
@@ -17,9 +17,14 @@
  * along with this library.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include <libopencm3/cm3/nvic.h>
-#include <libopencm3/stm32/gpio.h>
 #include <libopencm3/stm32/rcc.h>
+#include <libopencm3/stm32/gpio.h>
+#include <libopencm3/stm32/dbgmcu.h>
+#include <libopencm3/cm3/nvic.h>
+#include <libopencm3/cm3/scs.h>
+#include <libopencm3/cm3/tpiu.h>
+#include <libopencm3/cm3/dwt.h>
+#include <libopencm3/cm3/itm.h>
 #include <libopencm3/usb/dwc/otg_fs.h>
 
 #include <stdio.h>
@@ -27,12 +32,20 @@
 
 #define ER_DEBUG
 #ifdef ER_DEBUG
-#define ER_DPRINTF(fmt, ...) \
-	do { printf(fmt, ## __VA_ARGS__); } while (0)
+#define ER_DPRINTF(fmt, ...)        \
+	do {                            \
+		printf(fmt, ##__VA_ARGS__); \
+	} while (0)
 #else
 #define ER_DPRINTF(fmt, ...) \
-	do { } while (0)
+	do {                     \
+	} while (0)
 #endif
+
+#define SWO_BAUDRATE          115200U
+#define ARM_LAR_ACCESS_ENABLE 0xc5acce55U
+
+static void swo_setup(void);
 
 int main(void)
 {
@@ -48,6 +61,9 @@ int main(void)
 	gpio_mode_setup(GPIOC, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO13);
 	gpio_set(GPIOC, GPIO13);
 
+	/* Bring SWO up so we can be heard */
+	swo_setup();
+
 	/* https://github.com/libopencm3/libopencm3/pull/1256#issuecomment-779424001 */
 	OTG_FS_GCCFG |= OTG_GCCFG_NOVBUSSENS | OTG_GCCFG_PWRDWN;
 	OTG_FS_GCCFG &= ~(OTG_GCCFG_VBUSBSEN | OTG_GCCFG_VBUSASEN);
@@ -56,9 +72,40 @@ int main(void)
 
 	ER_DPRINTF("bootup complete\n");
 	gpio_clear(GPIOC, GPIO13);
-	while (1) {
+	while (true) {
 		gadget0_run(usbd_dev);
 	}
-
 }
 
+static void swo_setup(void)
+{
+	/* Enable tracing in DEMCR */
+	SCS_DEMCR |= SCS_DEMCR_TRCENA;
+
+	/* Get the active clock frequency of the core and use that to calculate a divisor */
+	const uint32_t clock_frequency = rcc_ahb_frequency;
+	const uint32_t divisor = (clock_frequency / SWO_BAUDRATE) - 1U;
+	/* And configure the TPIU for 1-bit async trace (SWO) in Manchester coding */
+	TPIU_LAR = ARM_LAR_ACCESS_ENABLE;
+	TPIU_CSPSR = 1U; /* 1-bit mode */
+	TPIU_ACPR = divisor;
+	TPIU_SPPR = TPIU_SPPR_ASYNC_NRZ; //TPIU_SPPR_ASYNC_MANCHESTER;
+	/* Ensure that TPIU framing is off */
+	TPIU_FFCR &= ~TPIU_FFCR_ENFCONT;
+
+	/* Configure the DWT to provide the sync source for the ITM */
+	DWT_LAR = ARM_LAR_ACCESS_ENABLE;
+	DWT_CTRL |= 0x000003feU;
+	/* Enable access to the ITM registers and configure tracing output from the first stimulus port */
+	ITM_LAR = ARM_LAR_ACCESS_ENABLE;
+	/* User-level access to the first 8 ports */
+	ITM_TPR = 0x0000000fU;
+	ITM_TCR = ITM_TCR_ITMENA | ITM_TCR_SYNCENA | ITM_TCR_TXENA | ITM_TCR_SWOENA | (1U << 16U);
+	ITM_TER[0] = 1U;
+	ITM_TER[1] = 1U;
+	ITM_TER[2] = 1U;
+
+	/* Now tell the DBGMCU that we want trace enabled and mapped as SWO */
+	DBGMCU_CR &= ~DBGMCU_CR_TRACE_MODE_MASK;
+	DBGMCU_CR |= DBGMCU_CR_TRACE_IOEN | DBGMCU_CR_TRACE_MODE_ASYNC;
+}
